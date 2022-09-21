cad9cee0035dfca64cb417b76bb3c0e1b5e3a535
Abu Sayeed <abusayeed33343536@gmail.com>
2022-09-21 11:32:59 +0600

    cache

---
+++ bootstrap/cache/gitlab_module.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\Gitlab\\Providers\\GitlabServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\Gitlab\\Providers\\GitlabServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
