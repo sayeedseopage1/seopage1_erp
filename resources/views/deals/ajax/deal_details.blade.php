e9cd69c9ceef562920b36d70cc70681927eea9ee
Abu Sayeed <sayeedseopage1@gmail.com>
2023-01-18 14:03:21 +0600

    project dashboard

--- resources/views/deals/ajax/deal_details.blade.php
+++ resources/views/deals/ajax/deal_details.blade.php
@@ -1538,6 +1538,62 @@ Swal.fire({
  // });
  })()
  </script>
+ @if (count($errors) > 0)
+ <script>
+     $( document ).ready(function() {
+         $('#dealaddstagemodal').modal('show');
+     });
+ </script>
+   @endif
+   <script type="text/javascript">
+   $(document).ready(function()
+   {
+     $('#date').bootstrapMaterialDatePicker
+     ({
+       time: false,
+       clearButton: true
+     });
+
+     $('#time').bootstrapMaterialDatePicker
+     ({
+       date: false,
+       shortTime: false,
+       format: 'HH:mm'
+     });
+
+     $('#date-format').bootstrapMaterialDatePicker
+     ({
+       format: 'DD MMMM YYYY HH:mm:ss'
+     });
+     $('#date-fr').bootstrapMaterialDatePicker
+     ({
+       format: 'DD/MM/YYYY HH:mm',
+       lang: 'fr',
+       weekStart: 1,
+       cancelText : 'ANNULER',
+       nowButton : true,
+       switchOnClick : true
+     });
+
+     $('#date-end').bootstrapMaterialDatePicker
+     ({
+       weekStart: 0, format: 'DD/MM/YYYY HH:mm'
+     });
+     $('#date-start').bootstrapMaterialDatePicker
+     ({
+       weekStart: 0, format: 'DD/MM/YYYY HH:mm', shortTime : true
+     }).on('change', function(e, date)
+     {
+       $('#date-end').bootstrapMaterialDatePicker('setMinDate', date);
+     });
+
+     $('#min-date').bootstrapMaterialDatePicker({ format : 'DD/MM/YYYY HH:mm', minDate : new Date() });
+
+     $.material.init()
+   });
+
+
+   </script>
 
 
 
