cb6ca7ad67efc3a6126c066e3a444716830d6cc7
nazmul <nazmulseopage1@gmail.com>
2023-03-16 10:36:45 +0600

    update with won deals table

--- app/Models/Deal.php
+++ app/Models/Deal.php
@@ -11,22 +11,26 @@ use App\Models\Lead;
 class Deal extends Model
 {
     use HasFactory;
-      protected $table = 'deals';
-      public function original_currency()
-      {
-          return $this->belongsTo(Currency::class, 'original_currency_id');
-      }
-        public function user()
-      {
-          return $this->belongsTo(User::class, 'added_by');
-      }
-      public function pm()
+    protected $table = 'deals';
+
+    public function original_currency()
+    {
+        return $this->belongsTo(Currency::class, 'original_currency_id');
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'added_by');
+    }
+    public function pm()
     {
         return $this->belongsTo(User::class, 'pm_id');
     }
     public function lead()
-  {
-      return $this->belongsTo(Lead::class, 'lead_id');
-  }
-
+    {
+        return $this->belongsTo(Lead::class, 'lead_id');
+    }
+    public function addedBy()
+    {
+        return $this->belongsTo(User::class, 'added_by');
+    }
 }
