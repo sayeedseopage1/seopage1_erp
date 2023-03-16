e757574e849deee032e8a981fa7a3c31e693f744
nazmul <nazmulseopage1@gmail.com>
2023-03-16 16:19:42 +0600

    order table fixed for project and deals table

--- app/Traits/EmployeeDashboard.php
+++ app/Traits/EmployeeDashboard.php
@@ -41,7 +41,7 @@ trait EmployeeDashboard
      */
     public function employeeDashboard()
     {
-        dd(request()->all());
+        // dd(request()->all());
       $this->startDate  = (request('startDate') != '') ? Carbon::createFromFormat($this->global->date_format, request('startDate')) : now($this->global->timezone)->startOfMonth();
 
       $this->endDate = (request('endDate') != '') ? Carbon::createFromFormat($this->global->date_format, request('endDate')) : now($this->global->timezone);
