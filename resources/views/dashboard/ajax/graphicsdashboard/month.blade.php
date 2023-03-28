932c5bd8e130cb089279455d953b08421d88315e
nazmul <nazmulseopage1@gmail.com>
2023-03-28 16:18:33 +0600

    update with qc form

--- resources/views/dashboard/ajax/graphicsdashboard/month.blade.php
+++ resources/views/dashboard/ajax/graphicsdashboard/month.blade.php
@@ -77,7 +77,7 @@
     <div class="col-sm-12 col-lg-6 mt-3">
         <div class="card bg-white border-0 b-shadow-4">
             <div class="card-header bg-white border-0 text-capitalize d-flex justify-content-between p-20">
-                <h4 class="f-18 f-w-500 mb-0">Total Task Assigned On Graphics Developer (To Do) 
+                <h4 class="f-18 f-w-500 mb-0">Total Task Assigned On Graphics Designer (To Do) 
                     <svg class="svg-inline--fa fa-question-circle fa-w-16" data-toggle="popover" data-placement="top" data-content="From 01-03-2023 To 23-03-2023" data-trigger="hover" aria-hidden="true" focusable="false" data-prefix="fa" data-icon="question-circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg="" data-original-title="" title=""><path fill="currentColor" d="M504 256c0 136.997-111.043 248-248 248S8 392.997 8 256C8 119.083 119.043 8 256 8s248 111.083 248 248zM262.655 90c-54.497 0-89.255 22.957-116.549 63.758-3.536 5.286-2.353 12.415 2.715 16.258l34.699 26.31c5.205 3.947 12.621 3.008 16.665-2.122 17.864-22.658 30.113-35.797 57.303-35.797 20.429 0 45.698 13.148 45.698 32.958 0 14.976-12.363 22.667-32.534 33.976C247.128 238.528 216 254.941 216 296v4c0 6.627 5.373 12 12 12h56c6.627 0 12-5.373 12-12v-1.333c0-28.462 83.186-29.647 83.186-106.667 0-58.002-60.165-102-116.531-102zM256 338c-25.365 0-46 20.635-46 46 0 25.364 20.635 46 46 46s46-20.636 46-46c0-25.365-20.635-46-46-46z"></path></svg>
                 </h4>
             </div>
@@ -160,7 +160,7 @@
     <div class="col-sm-12 col-lg-6 mt-3">
         <div class="card bg-white border-0 b-shadow-4">
             <div class="card-header bg-white border-0 text-capitalize d-flex justify-content-between p-20">
-                <h4 class="f-18 f-w-500 mb-0">Total Task Assigned On Graphics Developer (Doing) 
+                <h4 class="f-18 f-w-500 mb-0">Total Task Assigned On Graphics Designer (Doing) 
                     <svg class="svg-inline--fa fa-question-circle fa-w-16" data-toggle="popover" data-placement="top" data-content="From 01-03-2023 To 23-03-2023" data-trigger="hover" aria-hidden="true" focusable="false" data-prefix="fa" data-icon="question-circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg="" data-original-title="" title=""><path fill="currentColor" d="M504 256c0 136.997-111.043 248-248 248S8 392.997 8 256C8 119.083 119.043 8 256 8s248 111.083 248 248zM262.655 90c-54.497 0-89.255 22.957-116.549 63.758-3.536 5.286-2.353 12.415 2.715 16.258l34.699 26.31c5.205 3.947 12.621 3.008 16.665-2.122 17.864-22.658 30.113-35.797 57.303-35.797 20.429 0 45.698 13.148 45.698 32.958 0 14.976-12.363 22.667-32.534 33.976C247.128 238.528 216 254.941 216 296v4c0 6.627 5.373 12 12 12h56c6.627 0 12-5.373 12-12v-1.333c0-28.462 83.186-29.647 83.186-106.667 0-58.002-60.165-102-116.531-102zM256 338c-25.365 0-46 20.635-46 46 0 25.364 20.635 46 46 46s46-20.636 46-46c0-25.365-20.635-46-46-46z"></path></svg>
                 </h4>
             </div>
@@ -245,7 +245,7 @@
     <div class="col-sm-12 col-lg-6 mt-3">
         <div class="card bg-white border-0 b-shadow-4">
             <div class="card-header bg-white border-0 text-capitalize d-flex justify-content-between p-20">
-                <h4 class="f-18 f-w-500 mb-0">Total Task Assigned On Graphics Developer (Overdue) 
+                <h4 class="f-18 f-w-500 mb-0">Total Task Assigned On Graphics Designer (Overdue) 
                     <svg class="svg-inline--fa fa-question-circle fa-w-16" data-toggle="popover" data-placement="top" data-content="From 01-03-2023 To 23-03-2023" data-trigger="hover" aria-hidden="true" focusable="false" data-prefix="fa" data-icon="question-circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg="" data-original-title="" title=""><path fill="currentColor" d="M504 256c0 136.997-111.043 248-248 248S8 392.997 8 256C8 119.083 119.043 8 256 8s248 111.083 248 248zM262.655 90c-54.497 0-89.255 22.957-116.549 63.758-3.536 5.286-2.353 12.415 2.715 16.258l34.699 26.31c5.205 3.947 12.621 3.008 16.665-2.122 17.864-22.658 30.113-35.797 57.303-35.797 20.429 0 45.698 13.148 45.698 32.958 0 14.976-12.363 22.667-32.534 33.976C247.128 238.528 216 254.941 216 296v4c0 6.627 5.373 12 12 12h56c6.627 0 12-5.373 12-12v-1.333c0-28.462 83.186-29.647 83.186-106.667 0-58.002-60.165-102-116.531-102zM256 338c-25.365 0-46 20.635-46 46 0 25.364 20.635 46 46 46s46-20.636 46-46c0-25.365-20.635-46-46-46z"></path></svg>
                 </h4>
             </div>
@@ -328,7 +328,7 @@
     <div class="col-sm-12 col-lg-6 mt-3">
         <div class="card bg-white border-0 b-shadow-4">
             <div class="card-header bg-white border-0 text-capitalize d-flex justify-content-between p-20">
-                <h4 class="f-18 f-w-500 mb-0">Total Task Assigned On Graphics Developer (Under Review) 
+                <h4 class="f-18 f-w-500 mb-0">Total Task Assigned On Graphics Designer (Under Review) 
                     <svg class="svg-inline--fa fa-question-circle fa-w-16" data-toggle="popover" data-placement="top" data-content="From 01-03-2023 To 23-03-2023" data-trigger="hover" aria-hidden="true" focusable="false" data-prefix="fa" data-icon="question-circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg="" data-original-title="" title=""><path fill="currentColor" d="M504 256c0 136.997-111.043 248-248 248S8 392.997 8 256C8 119.083 119.043 8 256 8s248 111.083 248 248zM262.655 90c-54.497 0-89.255 22.957-116.549 63.758-3.536 5.286-2.353 12.415 2.715 16.258l34.699 26.31c5.205 3.947 12.621 3.008 16.665-2.122 17.864-22.658 30.113-35.797 57.303-35.797 20.429 0 45.698 13.148 45.698 32.958 0 14.976-12.363 22.667-32.534 33.976C247.128 238.528 216 254.941 216 296v4c0 6.627 5.373 12 12 12h56c6.627 0 12-5.373 12-12v-1.333c0-28.462 83.186-29.647 83.186-106.667 0-58.002-60.165-102-116.531-102zM256 338c-25.365 0-46 20.635-46 46 0 25.364 20.635 46 46 46s46-20.636 46-46c0-25.365-20.635-46-46-46z"></path></svg>
                 </h4>
             </div>
