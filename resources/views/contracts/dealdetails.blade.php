4e34c9acd9e197889f535043de00c6b63e4dd82a
Md Rayhan Ali <78335557+rayhan2001@users.noreply.github.com>
2023-03-30 15:20:38 +0600

    Deal Stage Change Done

--- resources/views/contracts/dealdetails.blade.php
+++ resources/views/contracts/dealdetails.blade.php
@@ -1,8 +1,6 @@
 @extends('layouts.app')
 
 @section('filter-section')
-{{--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">--}}
-{{--<link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">--}}
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 <style>
 .milestone-wrapper{
