ab67974bcb480211a74e74157f4453a9b93b32bb
Bijoy-seopage1 <bijoy.seopage1@gmail.com>
2023-04-01 11:22:37 +0600

    won deals client searching issue added

--- resources/views/contracts/modals/dealaddmodal.blade.php
+++ resources/views/contracts/modals/dealaddmodal.blade.php
@@ -5,16 +5,21 @@
 <script type="text/javascript" src="{{asset('time-picker2/js/bootstrap-material-datetimepicker.js')}}"></script>
     <style media="screen">
     #timerss {
-    /* max-width: a; */
-    display: flex;
-    flex-direction: column;
-    margin: 8px auto;
-    /* background: aliceblue; */
-    align-items: flex-start;
-    border-radius: 7px;
-    justify-content: center;
-    /* padding: 7px; */
-    }
+      /* max-width: a; */
+      display: flex;
+      flex-direction: column;
+      margin: 8px auto;
+      /* background: aliceblue; */
+      align-items: flex-start;
+      border-radius: 7px;
+      justify-content: center;
+      /* padding: 7px; */
+      }
+      .existingClientSuccess {
+          background: #fff !IMPORTANT;
+          color: green !important;
+          border-radius: 50%;
+      }
     </style>
 <div class="modal fade" id="dealaddmodal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
@@ -49,8 +54,15 @@
                               <path fill="currentColor" d="M504 256c0 136.997-111.043 248-248 248S8 392.997 8 256C8 119.083 119.043 8 256 8s248 111.083 248 248zM262.655 90c-54.497 0-89.255 22.957-116.549 63.758-3.536 5.286-2.353 12.415 2.715 16.258l34.699 26.31c5.205 3.947 12.621 3.008 16.665-2.122 17.864-22.658 30.113-35.797 57.303-35.797 20.429 0 45.698 13.148 45.698 32.958 0 14.976-12.363 22.667-32.534 33.976C247.128 238.528 216 254.941 216 296v4c0 6.627 5.373 12 12 12h56c6.627 0 12-5.373 12-12v-1.333c0-28.462 83.186-29.647 83.186-106.667 0-58.002-60.165-102-116.531-102zM256 338c-25.365 0-46 20.635-46 46 0 25.364 20.635 46 46 46s46-20.636 46-46c0-25.365-20.635-46-46-46z"></path>
                           </svg>
                       </label>
-                      <input name="user_name" id="user_name" type="text" class="form-control height-35 f-14" placeholder="Enter Client Username">
-                      <label id="userNameError" class="error text-danger" for="user_name"></label>
+                      <div class="row">
+                          <div class="col-md-10">
+                              <input type="text" class="form-control height-35 f-14" placeholder="Enter Client Username" value="" name="client_username" id="client_username" autocomplete="off">
+                          </div>
+                          <div class="col-md-2 m-auto" title="Existing Client">
+                              <i class="fa fa-check-circle fa-2x existingClientSuccess" style="display: none;"></i>
+                              <i class="fa fa-plus-circle fa-2x existingClientAdded" style="display: none;"></i>
+                          </div>
+                      </div>
                   </div>
               </div>
           </div>
@@ -269,36 +281,71 @@
 
 
   </script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.1/bootstrap3-typeahead.min.js"></script>
-  <script type="text/javascript">
-      var path = "{{ route('client-search') }}";
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.1/bootstrap3-typeahead.min.js"></script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#client_name').on('paste', function(e) {
+            setTimeout(function() {
+                var trimmedValue = $('#client_name').val().trim();
+                var inputVal = '';
+                trimmedValue.split('').forEach(function(char, index) {
+                    setTimeout(function() {
+                        inputVal += char;
+                        $('#client_name').val(inputVal);
+                    }, index * 10); // Adjust the delay time as needed
+                });
+            }, 0);
+        });
+    });
 
-      $('.client-search').typeahead({
-          source: function (query, process) {
-              return $.get(path, {
-                  query: query
-              }, function (data) {
-                  var results = data.map(function(item){
-                      return '<div class="my-custom-typeahead" name="'+item.name+'" username="'+item.user_name+'"><button class="getData">'+ item.name +' ('+item.user_name+' )</button></div>';
-                  });
-                  return process(results);
-              });
-          },
-          highlighter: function (item) {
-              return item;
-          },
-          updater: function (item) {
-              var text = $(item).text(); // extract text content of button element
-              this.$element.val('text'); // set value of input field to extracted text
-              getData($(item).attr('name'), $(item).attr('username'))
-              // $('#client_username').val($(item).attr('username'));
-              return $(item).attr('name'); // return extracted text to highlighter function
-          }
-      })
+    $('#client_name').keypress(function() {
+        $('#client_username').val('');
+        $('.existingClientSuccess').hide();
+        $('.existingClientAdded').show();
+    });
 
-      function getData(name, username) {
-          $('#client_name').val(name);
-          $('#user_name').val(username);
-      }
-  </script>
+    $('#client_name').keyup(function() {
+        $('#client_username').val('');
+        $('.existingClientSuccess').hide();
+        $('.existingClientAdded').show();
+    });
+
+    var path = "{{ route('client-search') }}";
+
+    $('.client-search').typeahead({
+        source: function (query, process) {
+            return $.get(path, {
+                query: $.trim(query)
+            }, function (data) {
+                var results = data.map(function(item){
+                    if (item.name.toLowerCase() == $('#client_name').val().toLowerCase() || item.user_name.toLowerCase() == $('#client_name').val().toLowerCase()) {
+                        $('.existingClientSuccess').show();
+                        getData(item.name, item.user_name);
+                        return '';
+                    } else {
+                        return '<div class="my-custom-typeahead" name="'+item.name+'" username="'+item.user_name+'"><button class="getData">'+ item.name +' ('+item.user_name+' )</button></div>';
+                    }
+                });
+                return process(results);
+            });
+        },
+        highlighter: function (item) {
+            return item;
+        },
+        updater: function (item) {
+            var text = $(item).text(); // extract text content of button element
+            this.$element.val('text'); // set value of input field to extracted text
+            getData($(item).attr('name'), $(item).attr('username'))
+            // $('#client_username').val($(item).attr('username'));
+            return $.trim($(item).attr('name')); // return extracted text to highlighter function
+        }
+    })
+
+    function getData(name, username) {
+        $('#client_name').val(name);
+        $('#client_username').val(username);
+        $('.existingClientSuccess').show();
+        $('.existingClientAdded').hide();
+    }
+</script>
 @endpush
