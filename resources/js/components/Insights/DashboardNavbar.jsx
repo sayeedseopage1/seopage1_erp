48801c4db4c9a903434d87556093f512a8d2a720
MD Mehedi Hasan <mehedihasan.hr.324@gmail.com>
2023-04-18 11:22:22 +0600

    time period filter button created

--- resources/js/components/Insights/DashboardNavbar.jsx
+++ resources/js/components/Insights/DashboardNavbar.jsx
@@ -2,6 +2,8 @@ import { forwardRef } from "react";
 import Dropdown from "../UI/Dropdown";
 import React, { useEffect, useRef, useState } from "react";
 import ReactDatePicker from "react-datepicker";
+import PeriodFilter from "./components/PeriodFilter";
+import Selection from "../UI/form/Selection";
 
 const DashboardNavbar = () => {
     const [dashboardName, setDashboardName] = useState("My Dashboard");
@@ -37,83 +39,9 @@ const DashboardNavbar = () => {
             <div className="d-flex align-items-center">
                 <div className="d-flex">
                     {/* time period filter button */}
-                    <Dropdown onClickHide={false}>
-                        <Dropdown.Toggle
-                            icon={false}
-                            className="sp1_d--time-filter-toggle"
-                        >
-                            <div className="d-flex sp1_d--time-filter">
-                                <div className="sp1_d--time-tt">
-                                    <i className="fa-solid fa-calendar-day"></i>
-                                    <span> Period </span>
-                                    <i className="fa-solid fa-caret-down" />
-                                </div>
-                                <button aria-label="close">
-                                    <i className="fa-solid fa-xmark"></i>
-                                </button>
-                            </div>
-                        </Dropdown.Toggle>
-                        <Dropdown.Menu className="sp1_d--time-filter-menu">
-                            {/* Dropdown */}
-                            <Dropdown>
-                                <Dropdown.Toggle className="sp1_d--time-filter py-2 px-3">
-                                    Tomorrow
-                                </Dropdown.Toggle>
-                                <Dropdown.Menu className="">
-                                    <React.Fragment>
-                                        <ul>
-                                            <li>Today</li>
-                                            <li>YesterDay</li>
-                                        </ul>
-                                    </React.Fragment>
-                                </Dropdown.Menu>
-                            </Dropdown>
-                            {/* end dropdown */}
-
-                            <div>
-                                <div className="sp1_sell_df--control">
-                                    <div className="w-100">
-                                        <ReactDatePicker
-                                            selected={startDate}
-                                            onChange={(date) =>
-                                                setStartDate(date)
-                                            }
-                                            selectsStart
-                                            startDate={startDate}
-                                            endDate={endDate}
-                                            selectsDisabledDaysInRange
-                                            customInput={<DateInput />}
-                                        />
-                                    </div>
-                                    <span>-</span>
-                                    <div className="position-relative w-100">
-                                        <ReactDatePicker
-                                            selected={endDate}
-                                            onChange={(date) =>
-                                                setEndDate(date)
-                                            }
-                                            selectsEnd
-                                            disabledKeyboardNavigation
-                                            startDate={startDate}
-                                            endDate={endDate}
-                                            minDate={startDate}
-                                            placeholderText="No end date"
-                                            selectsDisabledDaysInRange
-                                            customInput={<DateInput />}
-                                        />
-                                        <button
-                                            onClick={() => setDurationEnd(null)}
-                                            className="sp1_clear_time"
-                                            aria-describedby="clearEndDate"
-                                        >
-                                            &times;
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </Dropdown.Menu>
-                    </Dropdown>
+                    <PeriodFilter />
                     {/* end time period filter button */}
+                    <Selection />
                 </div>
             </div>
         </div>
