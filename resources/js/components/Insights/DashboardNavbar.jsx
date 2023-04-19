6edcbc0677596043779a352c74ade109ea5b0265
MD Mehedi Hasan <mehedihasan.hr.324@gmail.com>
2023-04-19 13:22:35 +0600

    time log table

--- resources/js/components/Insights/DashboardNavbar.jsx
+++ resources/js/components/Insights/DashboardNavbar.jsx
@@ -1,9 +1,10 @@
 import { forwardRef } from "react";
-import Dropdown from "../UI/Dropdown";
+import Dropdown from "./components/Dropdown";
 import React, { useEffect, useRef, useState } from "react";
 import ReactDatePicker from "react-datepicker";
 import PeriodFilter from "./components/PeriodFilter";
 import UsersFilterDropdown from "./components/UsersFilterDropdown";
+import FilterButton from "./components/FilterButton";
 
 const DashboardNavbar = () => {
     const [dashboardName, setDashboardName] = useState("My Dashboard");
@@ -39,36 +40,74 @@ const DashboardNavbar = () => {
             </div>
 
             <div className="d-flex align-items-center">
-                <div className="d-flex">
+                <div className="d-flex border-right">
                     {/* time period filter button */}
-                    <PeriodFilter
-                        selectedPeriod={selectedPeriod}
-                        setSelectedPeriod={setSelectedPeriod}
-                        startDate={startDate}
-                        endDate={endDate}
-                        setStartDate={setStartDate}
-                        setEndDate={setEndDate}
-                    />
-                    <UsersFilterDropdown
-                        filteredUser={filteredUser}
-                        setFilteredUser={setFilteredUser}
-                    />
+                    <div className="mr-2">
+                        <PeriodFilter
+                            selectedPeriod={selectedPeriod}
+                            setSelectedPeriod={setSelectedPeriod}
+                            startDate={startDate}
+                            endDate={endDate}
+                            setStartDate={setStartDate}
+                            setEndDate={setEndDate}
+                        />
+                    </div>
+                    <div className="mr-3">
+                        <UsersFilterDropdown
+                            filteredUser={filteredUser}
+                            setFilteredUser={setFilteredUser}
+                        />
+                    </div>
+                </div>
+
+                <div className="d-flex ml-3">
+                    {/* time period filter button */}
+                    <div className="mr-2">
+                        <FilterButton>
+                            <i className="bi bi-share" />
+                            <span>Share</span>
+                        </FilterButton>
+                    </div>
+                    <div className="mr-2">
+                        <Dropdown>
+                            <Dropdown.Toggle className="sp1_db--export">
+                                <i className="bi bi-printer-fill" />
+                                <span>Export</span>
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu className="sp1_db--export-menu">
+                                <Dropdown.Item>PDF</Dropdown.Item>
+                                <Dropdown.Item>PNG</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </div>
+
+                    <div className="mr-2">
+                        <Dropdown>
+                            <Dropdown.Toggle
+                                icon={false}
+                                className="sp1_db--export"
+                            >
+                                <i className="fa-solid fa-ellipsis-vertical"></i>
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu className="sp1_db--export-menu">
+                                <Dropdown.Item>
+                                    <div className="d-flex align-items-center">
+                                        <i className="fa-solid fa-copy mr-2"></i>
+                                        <span>Duplicate</span>
+                                    </div>
+                                </Dropdown.Item>
+                                <Dropdown.Item>
+                                    <div className="d-flex align-items-center">
+                                        <i className="bi bi-trash mr-2" />
+                                        <span>Delete</span>
+                                    </div>
+                                </Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </div>
                 </div>
             </div>
         </div>
     );
 };
 export default DashboardNavbar;
-
-const DateInput = forwardRef(
-    ({ value, placeholder, onClick, ...props }, ref) => (
-        <button
-            className="sp1_selection--toggle sp1_sell_df--datepicker"
-            onClick={onClick}
-            ref={ref}
-            style={{ gap: 0 }}
-        >
-            {value || placeholder}
-        </button>
-    )
-);
--- resources/js/components/Insights/components/UsersFilterDropdown.jsx
+++ resources/js/components/Insights/components/UsersFilterDropdown.jsx
@@ -1,20 +1,26 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { usePopper } from "react-popper";
-import RelativeDateFilter from "./RelativeDateFilter";
 import "./period-filter.css";
-import TimeRange from "../../UI/TimeRange";
-import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import Search from "../../UI/form/Search";
+import { addAssigneeUsers, addAssigneeTeams } from "../services/assigneeSlice";
+import CustomScrollbar from "../../UI/CustomScrollbar";
+import _ from "lodash";
 
 const UsersFilterDropdown = ({ filteredUser, setFilteredUser }) => {
     const { users, teams } = useSelector((s) => s.assignee);
+    const dispatch = useDispatch();
     const [isOpen, setIsOpen] = useState(false);
     const [title, setTitle] = useState("User");
+    const [search, setSearch] = useState("");
     const [refElement, setRefElement] = useState(null);
     const [popperElement, setPopperElement] = useState(null);
-
     const [closeButton, setCloseButton] = useState(false);
 
+    const [defaultHover, setDefaultHover] = useState("");
+    const [fetching, setFetching] = useState(true);
+
+    // popper
     const { styles, attributes } = usePopper(refElement, popperElement, {
         placement: "bottom-start",
         modifiers: [
@@ -22,6 +28,36 @@ const UsersFilterDropdown = ({ filteredUser, setFilteredUser }) => {
             { name: "offset", options: { offset: [0, 3] } },
         ],
     });
+
+    // get user
+    useEffect(() => {
+        if (users.length === 0) {
+            setFetching(true);
+            const fetchUser = async () => {
+                const res = await axios.get("/get-users");
+                if (res) {
+                    dispatch(addAssigneeUsers(res.data));
+                    setFetching(false);
+                }
+            };
+
+            fetchUser();
+        }
+
+        if (teams.length === 0) {
+            // teams fetching here....
+            const fetchTeam = async () => {
+                setFetching(true);
+                const res = await axios.get("/get-teams");
+                if (res) {
+                    dispatch(addAssigneeTeams(res.data));
+                    setFetching(false);
+                }
+            };
+
+            fetchTeam();
+        }
+    }, []);
 
     // close outside click
     useEffect(() => {
@@ -48,6 +84,7 @@ const UsersFilterDropdown = ({ filteredUser, setFilteredUser }) => {
         };
     }, [popperElement]);
 
+    // clear state
     const clearState = () => {
         setTitle("Users");
         setCloseButton(false);
@@ -56,6 +93,7 @@ const UsersFilterDropdown = ({ filteredUser, setFilteredUser }) => {
     // handle time selection
     const handleTimePeriodSelect = (v) => {
         setTitle(v);
+        setFilteredUser(v);
         setCloseButton(true);
     };
 
@@ -85,9 +123,131 @@ const UsersFilterDropdown = ({ filteredUser, setFilteredUser }) => {
                     style={styles.popper}
                     {...attributes}
                 >
-                    {users.length > 0 ? (
-                        <React.Fragment></React.Fragment>
-                    ) : null}
+                    <React.Fragment>
+                        <Search value={search} onChange={setSearch} />
+                        <CustomScrollbar minH={0} maxH={450}>
+                            <div className="mb-3 mr-2">
+                                {fetching ? (
+                                    <>Loading...</>
+                                ) : (
+                                    <>
+                                        <ul className="sp1_user_filter--list mt-3">
+                                            <li
+                                                className={`sp1__asf--item sp1_user_filter--item ${
+                                                    defaultHover === "Everyone"
+                                                        ? "hover"
+                                                        : ""
+                                                } `}
+                                                onMouseOver={() =>
+                                                    setDefaultHover("Everyone")
+                                                }
+                                                onClick={() =>
+                                                    handleTimePeriodSelect(
+                                                        "Everyone"
+                                                    )
+                                                }
+                                            >
+                                                <span>Everyone</span>
+                                                {filteredUser ===
+                                                    "Everyone" && (
+                                                    <i className="fa-solid fa-check" />
+                                                )}
+                                            </li>
+                                        </ul>
+                                        <hr className="my-2" />
+                                        <ul className="sp1_user_filter--list">
+                                            {users.length > 0
+                                                ? users
+                                                      .filter((u) =>
+                                                          _.lowerCase(
+                                                              u.name
+                                                          ).includes(
+                                                              _.lowerCase(
+                                                                  search
+                                                              )
+                                                          )
+                                                      )
+                                                      .map((u) => (
+                                                          <li
+                                                              key={u.id}
+                                                              className={`sp1__asf--item sp1_user_filter--item ${
+                                                                  defaultHover ===
+                                                                  u.name
+                                                                      ? "hover"
+                                                                      : ""
+                                                              } `}
+                                                              onMouseOver={() =>
+                                                                  setDefaultHover(
+                                                                      u.name
+                                                                  )
+                                                              }
+                                                              onClick={() =>
+                                                                  handleTimePeriodSelect(
+                                                                      u.name
+                                                                  )
+                                                              }
+                                                          >
+                                                              <span>
+                                                                  {" "}
+                                                                  {u.name}
+                                                              </span>
+                                                              {u.name ===
+                                                                  filteredUser && (
+                                                                  <i className="fa-solid fa-check" />
+                                                              )}
+                                                          </li>
+                                                      ))
+                                                : null}
+                                        </ul>
+                                        <hr className="my-2" />
+                                        <ul className="sp1_user_filter--list">
+                                            {teams.length > 0
+                                                ? teams
+                                                      .filter((t) =>
+                                                          _.lowerCase(
+                                                              t.team_name
+                                                          ).includes(
+                                                              _.lowerCase(
+                                                                  search
+                                                              )
+                                                          )
+                                                      )
+                                                      .map((t) => (
+                                                          <li
+                                                              key={`team-${t.id}`}
+                                                              className={`sp1__asf--item sp1_user_filter--item ${
+                                                                  defaultHover ===
+                                                                  t.team_name
+                                                                      ? "hover"
+                                                                      : ""
+                                                              } `}
+                                                              onClick={() =>
+                                                                  handleTimePeriodSelect(
+                                                                      t.team_name
+                                                                  )
+                                                              }
+                                                              onMouseOver={() =>
+                                                                  setDefaultHover(
+                                                                      t.team_name
+                                                                  )
+                                                              }
+                                                          >
+                                                              <span>
+                                                                  {t.team_name}
+                                                              </span>
+                                                              {t.team_name ===
+                                                                  filteredUser && (
+                                                                  <i className="fa-solid fa-check" />
+                                                              )}
+                                                          </li>
+                                                      ))
+                                                : null}
+                                        </ul>
+                                    </>
+                                )}
+                            </div>
+                        </CustomScrollbar>
+                    </React.Fragment>
                 </div>
             )}
         </div>
