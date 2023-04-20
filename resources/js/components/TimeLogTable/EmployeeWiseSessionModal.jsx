6316a9949ce6b5667be0d9d5138a908ac6d35e8e
MD Mehedi Hasan <mehedihasan.hr.324@gmail.com>
2023-04-20 14:44:35 +0600

    log table

---
+++ resources/js/components/TimeLogTable/EmployeeWiseSessionModal.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import Modal from '../UI/Modal';
+
+
+const EmployeeWiseSessionModal = () => {
+    const [isOpen, setIsOpen] = useState();
+    return(
+        <Modal isOpen={true}>
+            <div className=''>
+                 
+            </div>
+        </Modal>
+    )
+}
