c021ad0f401a4a5dfc0fc2dfa456298dd332f6f3
MD Mehedi Hasan <mehedihasan.hr.324@gmail.com>
2023-05-03 15:34:47 +0600

    update

--- resources/js/components/Insights/hooks/useGoals.jsx
+++ resources/js/components/Insights/hooks/useGoals.jsx
@@ -5,14 +5,17 @@ import * as React from 'react';
 
 export const useGoals = () => {
     const {goals}  = useSelector(state => state.goals);
+    
 
     // fetch all goals
     React.useEffect(() => {
         const fetch = async () => {
+            const id = window.Laravel.user.id;
+            if(!id) return;
             // dispatch(setStatus({status: "loading"}));
-            await axios.get('/account/insights/goals/get').then(res => {
+            await axios.get(`/account/insights/goals/get/${id}`).then(res => {
                 // dispatch(setDeals(res.data));
-                // console.log(res.data);
+                console.log(res.data);
                 // dispatch(setStatus({status: 'completed'}));
             }).catch(err => {
                 console.log(err);
