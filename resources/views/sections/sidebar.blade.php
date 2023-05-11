b16d3bf440913a6d2a6609685dc69b90ce152994
sayeedseopage1 <113491743+sayeedseopage1@users.noreply.github.com>
2023-05-11 16:08:03 +0600

    migration fixed

--- resources/views/sections/sidebar.blade.php
+++ resources/views/sections/sidebar.blade.php
@@ -289,7 +289,7 @@
                             <x-sub-menu-item :link="route('contracts.index')" :text="'Won Deals'" />
                             {{-- <x-sub-menu-item link="" :text="'Qualified Sales'" /> --}}
                             <x-sub-menu-item :link="route('insights.index')" :text="'Goals & Insights'" />
-                            {{-- <x-sub-menu-item :link="route('points.index')" :text="'Points'" /> --}}
+                            <x-sub-menu-item :link="route('points.index')" :text="'Points'" />
                             {{-- <x-sub-menu-item link="" :text="'Incentives'" /> --}}
                         </div>
                     </x-menu-item>
