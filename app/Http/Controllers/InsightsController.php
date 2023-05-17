1776b2d580792432b6c68c9fc401df8b28823cb7
Bijoy-seopage1 <bijoy.seopage1@gmail.com>
2023-05-17 12:24:41 +0600

    udpate with goal logic

--- app/Http/Controllers/InsightsController.php
+++ app/Http/Controllers/InsightsController.php
@@ -15,6 +15,7 @@ use App\Models\Lead;
 use Illuminate\Support\Facades\DB;
 use Auth;
 use App\Models\Deal;
+use App\Models\DealStageChange;
 
 class InsightsController extends AccountBaseController
 {
@@ -626,6 +627,7 @@ class InsightsController extends AccountBaseController
             $data2 = $data->user_id ? [$data->user_id] : $user_data;
            
             $dealStage = Deal::select([
+                'deals.id as id',
                 'deals.id as won_deal_id',
                 'deals.project_name as deal_project_name',
                 'deals.client_username as client_username',
@@ -660,9 +662,74 @@ class InsightsController extends AccountBaseController
             }
 
             $dealStage = $dealStage->get();
+
+            $deals_data = Deal::select([
+                'deals.*', 
+                'leads.added_by as bidder',
+            ])
+            ->join('leads', 'leads.id', 'deals.lead_id')
+            ->orderBy('deals.id', 'desc')
+            ->get();
+
+            foreach ($deals_data as $key => $value) {
+                $member = rtrim($data->goal->members, ',');
+                $member = explode(',', $member);
+                
+                $value->deal_stage = DealStageChange::where('deal_id', $value->deal_id)->get();
+                $value->bidder_amount = round((24 * $value->amount) / 100, 2);
+                $value->team_total_amount = 0;
+                $team_total_amount = 0;
+
+                foreach ($value->deal_stage as $key => $deal_stage) {
+                    $amount = 0;
+                    if ($deal_stage->deal_stage_id ==  1) {
+                        $value->qualified_by = $deal_stage->updated_by;
+                        $amount = round((4 * $value->amount) / 100, 2);
+                        $value->qualified_amount = $amount;
+                    } elseif ($deal_stage->deal_stage_id == 2) {
+                        $value->required_defined = $deal_stage->updated_by;
+                        $amount = round((17 * $value->amount) / 100, 2);
+                        $value->required_defined_amount = $amount;
+                    } elseif ($deal_stage->deal_stage_id == 3) {
+                        $value->proposal_made = $deal_stage->updated_by;
+                        $amount = round((12 * $value->amount) / 100, 2);
+                        $value->proposal_made_amount = $amount;
+                    } elseif ($deal_stage->deal_stage_id == 4) {
+                        $value->negotiation_started = $deal_stage->updated_by;
+                        $amount = round((12 * $value->amount) / 100, 2);
+                        $value->negotiation_started_amount = $amount;
+                    } elseif ($deal_stage->deal_stage_id == 5) {
+                        $value->milestone_breakdown = $deal_stage->updated_by;
+                        $amount = round((14 * $value->amount) / 100, 2);
+                        $value->milestone_breakdown_amount = $amount;
+                    }
+
+                    if (in_array($deal_stage->updated_by, $member)) {
+                        $team_total_amount = $team_total_amount + $amount;
+                        $value->team_total_amount = $team_total_amount;
+                    }
+                }
+
+                $value->won_deal_amount = round((17 * $value->amount) / 100, 2);
+                $team_summation = DealStageChange::where('deal_id', $value->deal_id)->whereIn('updated_by', $member)->get();
+
+                if (in_array($value->added_by, $member)) {
+                    //$team_total_amount = $team_total_amount + $amount;
+                    $value->team_total_amount = round($value->team_total_amount + $value->won_deal_amount, 2);
+                }
+
+                if (in_array($value->bidder, $member)) {
+                    //$team_total_amount = $team_total_amount + $amount;
+                    $value->team_total_amount = round($value->team_total_amount + $value->bidder_amount, 2);
+                }
+
+                $array[] = $value;
+            }
+            return response()->json($array);
         }
 
         return response()->json($dealStage);
+
     }
 
 
