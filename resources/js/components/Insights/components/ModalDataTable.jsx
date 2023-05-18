ff8025304979197bb809f863919d369aab9be42f
Md Mehed Hasan Hridoy <mehedihasanseopage1@gmail.com>
2023-05-18 15:32:47 +0600

    'update_sales'

--- resources/js/components/Insights/components/ModalDataTable.jsx
+++ resources/js/components/Insights/components/ModalDataTable.jsx
@@ -41,7 +41,7 @@ export const ModalDataTable = () =>{
     const getWonTableColumns = React.useCallback((goal) => {
         
         if(goal?.entryType === 'Won'){
-            if(goal?.general_checkbox === "1"){
+            if(Number(team_id) === 1){
                 let t = wonTableVisibleColumns.filter(
                     column => column.accessor !== 'team_total_amount'
                 )
