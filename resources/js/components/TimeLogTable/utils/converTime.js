c6555e21b6af196bc801085099db1ab9c6765fed
Md Mehed Hasan Hridoy <mehedihasanseopage1@gmail.com>
2023-05-23 16:45:37 +0600

    'update_time_log_table'

--- resources/js/components/TimeLogTable/utils/converTime.js
+++ resources/js/components/TimeLogTable/utils/converTime.js
@@ -3,8 +3,8 @@ export const convertTime = (time) => {
         const hours = Math.floor(time / 60) || 0;
         const min = Math.floor(time % 60) || 0;
 
-        const h = hours ? `${hours} hours` : "";
-        return `${h} ${min} min`;
+        const h = hours ? `${hours<10 ? `0${hours}`: hours} hours` : "";
+        return `${h} ${min < 10 ? `0${min}`: min } min`;
     }
 
     return `0 min`;
