b21b3a4d483e8a1d4ccee848ab033c84b8dc4591
Md Rayhan Ali <78335557+rayhan2001@users.noreply.github.com>
2023-05-24 11:28:43 +0600

    Approve cancelation bug fix done

--- app/Http/Controllers/IncentiveController.php
+++ app/Http/Controllers/IncentiveController.php
@@ -34,7 +34,7 @@ class IncentiveController extends AccountBaseController
 
     public function index_json(Request $request)
     {
-       // dd($request);
+        // dd($request);
         //dd($request);
         if (isset($request->start_date)) {
             $start_date = Carbon::parse($request->start_date)->format('Y-m-d');
@@ -44,11 +44,11 @@ class IncentiveController extends AccountBaseController
         }
         if (Auth::user()->role_id == 8 || Auth::user()->role_id == 7 ) {
             $userID = Auth::id();
-        }else 
+        }else
         {
             $userID = $request->user_id;
         }
-      
+
 
         $user_goals = GoalSetting::where([
             'assigneeType' => 'User',
@@ -61,11 +61,11 @@ class IncentiveController extends AccountBaseController
             'assigneeType' => 'Team',
             'goalType' => 'minimum'
         ])->get();
-        
+
         foreach ($team_goal as $value) {
             $members = $value->goal->members;
             $members = explode(',', rtrim($members, ','));
-            
+
             if (in_array($userID, $members)) {
                 $user_goals++;
             }
@@ -84,13 +84,13 @@ class IncentiveController extends AccountBaseController
             'assigneeType' => 'Team',
             'goalType' => 'minimum',
             'goal_status' => '1',
-            
+
         ])->get();
-        
+
         foreach ($team_achieve_goal as $value) {
             $members = $value->goal->members;
             $members = explode(',', rtrim($members, ','));
-            
+
             if (in_array($userID, $members)) {
                 $user_achieve_goals++;
             }
@@ -104,13 +104,13 @@ class IncentiveController extends AccountBaseController
         $minimum_team_goals = GoalSetting::where([
             'assigneeType' => 'Team',
             'goalType' => 'minimum'
-            
+
         ])->get();
-        
+
         foreach ($minimum_team_goals as $value) {
             $members = $value->goal->members;
             $members = explode(',', rtrim($members, ','));
-            
+
             if (in_array($userID, $members)) {
                 if ($value->goal_status == 1) {
                     $mimimum_team_achieve_goal = $mimimum_team_achieve_goal + 1;
@@ -126,7 +126,7 @@ class IncentiveController extends AccountBaseController
 
         $incentive_setting = IncentiveSetting::first();
         $data['non_incentive_point_above'] = $incentive_setting->every_shift_every_point_above;
-       // dd($data['non_incentive_point_above']);
+        // dd($data['non_incentive_point_above']);
 
         $user_list_for_point_achieve = Seopage1Team::where('id', '!=', 1)->get();
         //dd($user_list_for_point_achieve);
@@ -138,16 +138,16 @@ class IncentiveController extends AccountBaseController
                 foreach ($user_lists as $user_id) {
                     array_push($user_array, $user_id);
                 }
-            }  
+            }
         }
 
-        
+
 
         $user_array = array_unique($user_array);
-    //    / dd($user_array);
+        //    / dd($user_array);
 
         $cash_point = CashPoint::whereIn('user_id', $user_array)->whereBetween('created_at', [Carbon::now()->startOfMonth(), Carbon::now()->endOfMonth()])->sum('points');
-    //    / dd($cash_point);
+        //    / dd($cash_point);
 
         //if user can't complete 10 days goal
         $ten_days_incomplete_goal = GoalSetting::where([
@@ -156,8 +156,8 @@ class IncentiveController extends AccountBaseController
             'frequency' => '10 Days',
             'goal_status' => 0
         ])
-        ->whereBetween('created_at', [Carbon::now()->startOfMonth(), Carbon::now()->endOfMonth()])
-        ->get();
+            ->whereBetween('created_at', [Carbon::now()->startOfMonth(), Carbon::now()->endOfMonth()])
+            ->get();
 
         // $data['deduct_point_for_incomplete_goal'] = 0;
         // foreach ($ten_days_incomplete_goal as $value) {
@@ -167,34 +167,34 @@ class IncentiveController extends AccountBaseController
         //         $cash_point = $cash_point - $deduct_point;
         //     }
         // }
-        //if user can't complete 10 days goal end 
+        //if user can't complete 10 days goal end
 
         $data['every_shift_team_total_acheive'] = $cash_point;
-   // dd($data['every_shift_team_total_acheive']);
+        // dd($data['every_shift_team_total_acheive']);
 
         $cash_point_total = CashPoint::whereIn('user_id', $user_array)->whereBetween('created_at', [Carbon::now()->startOfMonth(), Carbon::now()->endOfMonth()])->count();
         // $cash_point_total_of_this_user = CashPoint::where('user_id', $request->user_id)->whereBetween('created_at', [Carbon::now()->startOfMonth(), Carbon::now()->endOfMonth()])->count();
         $cash_point_total_of_this_user = CashPoint::where('user_id', $request->user_id)->whereBetween('created_at', [Carbon::now()->startOfMonth(), Carbon::now()->endOfMonth()])->sum('points');
         // /$data['point_achieve_by_your_shift'] = $cash_point_total;
         $data['point_achieve_by_your_shift'] = round($cash_point,2);
-    // dd($data['point_achieve_by_your_shift'], $cash_point_total_of_this_user);
+        // dd($data['point_achieve_by_your_shift'], $cash_point_total_of_this_user);
         if ($cash_point > 0) {
             // $total_percentage_share_incentive = (100 * ($cash_point_total - $cash_point_total_of_this_user)) / $cash_point_total ;
             $total_percentage_share_incentive = (100 * ($cash_point - $cash_point_total_of_this_user)) / $cash_point ;
-           
+
             $total_percentage_share_incentive_of_this_user = 100 - $total_percentage_share_incentive;
-           // dd($total_percentage_share_incentive,$total_percentage_share_incentive_of_this_user);
+            // dd($total_percentage_share_incentive,$total_percentage_share_incentive_of_this_user);
 
             $data['toal_share_incentive'] = ($data['every_shift_team_total_acheive'] / 100) * $total_percentage_share_incentive;
         } else {
             $data['toal_share_incentive'] = 0;
         }
-       // dd($data['toal_share_incentive']);
-       $data['point_value']= $incentive_setting->point_of_value;
-       $data['percentage_of_share']= $total_percentage_share_incentive_of_this_user;
-        
-       // dd($data);
-        
+        // dd($data['toal_share_incentive']);
+        $data['point_value']= $incentive_setting->point_of_value;
+        $data['percentage_of_share']= $total_percentage_share_incentive_of_this_user;
+
+        // dd($data);
+
         return response()->json($data);
     }
 
@@ -265,5 +265,5 @@ class IncentiveController extends AccountBaseController
     }
 
 
-  
+
 }
