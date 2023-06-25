8f5c1c94426a99cc49b491d02af42d2531e1c287
sayeedseopage1 <113491743+sayeedseopage1@users.noreply.github.com>
2023-06-25 17:09:46 +0600

    project_portfolio

--- app/Console/Commands/KpiDistribution.php
+++ app/Console/Commands/KpiDistribution.php
@@ -510,6 +510,11 @@ class KpiDistribution extends Command
                             // Set other fields as needed
                             $point->save();
                             CashPoint::where('user_id', $total_points->user_id)->where('type','Single Deal Bonus')->delete();
+                            $updated_total_points=  CashPoint::where('user_id', $total_points->user_id)->orderBy('id','desc')->first();
+                            $total_points=  CashPoint::where('user_id', $total_points->user_id)->sum('points');
+                            $point_update= CashPoint::find($$updated_total_points->id);
+                            $point_update->total_points_earn = $total_points;
+                            $point_update->save();
                             }
                     
             
