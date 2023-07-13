48d9a8caf69bc0ff652d0d48e17f107e8301121d
sayeedseopage1 <113491743+sayeedseopage1@users.noreply.github.com>
2023-07-13 16:51:32 +0600

    hourly_milestone_bug_fixed

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -232,6 +232,14 @@ class PaymentController extends AccountBaseController
 
         $payment->status = 'complete';
         $payment->save();
+        $project_id_find = Project::where('id',$payment->project_id)->first();
+        $deal_id_find = Deal::where('id',$project_id_find)->first();
+        if($deal_id_find != null && $deal_id_find->project_type == 'hourly')
+        {
+            $deal_update = Deal::find($deal_id_find);
+            $deal_update->payment_date = $payment->paid_on;
+            $deal_update->save();
+        }
 
         //authorization action start
 
