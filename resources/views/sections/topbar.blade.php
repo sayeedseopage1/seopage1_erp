c099bc97ccd4130f958ec385577bde2541e54491
riad69 <109710326+riad69@users.noreply.github.com>
2023-07-18 17:12:00 +0600

    active_timer_added_on_header_section

--- resources/views/sections/topbar.blade.php
+++ resources/views/sections/topbar.blade.php
@@ -31,6 +31,33 @@
     <!-- NAVBAR RIGHT(SEARCH, ADD, NOTIFICATION, LOGOUT) START-->
 
     <div class="page-header-right float-right d-flex align-items-center">
+
+    @if (!is_null($selfActiveTimer))
+
+        <span class="border rounded f-14 py-2 px-2 d-none d-sm-block mr-3">
+            <span id="active-timer" class="mr-2">{{ $selfActiveTimer->timer }}</span>
+
+            @if (is_null($selfActiveTimer->activeBreak))
+                <a href="javascript:;" class="pause-active-timer mr-1 border-right" data-toggle="tooltip" data-original-title="{{ __('modules.timeLogs.pauseTimer') }}" data-time-id="{{ $selfActiveTimer->id }}">
+                    <i class="fa fa-pause-circle text-primary"></i>
+                </a>
+                <a href="javascript:;" class="stop-active-timer" data-toggle="tooltip" data-original-title="{{ __('modules.timeLogs.stopTimer') }}" data-time-id="{{ $selfActiveTimer->id }}">
+                    <i class="fa fa-stop-circle text-danger"></i>
+                </a>
+            @else
+                <a href="javascript:;" class="resume-active-timer" data-toggle="tooltip" data-original-title="{{ __('modules.timeLogs.resumeTimer') }}" data-time-id="{{ $selfActiveTimer->activeBreak->id }}">
+                    <i class="fa fa-play-circle text-primary"></i>
+                </a>
+            @endif
+
+        </span>
+
+        @if (is_null($selfActiveTimer->activeBreak))
+            <a href="javascript:;" class='btn-danger btn btn-sm rounded mr-3 f-14 py-2 px-2 stop-active-timer d-block d-sm-none mr-2' data-time-id="{{ $selfActiveTimer->id }}">
+                {{ __('modules.timeLogs.stopTimer') }}
+            </a>
+        @endif
+    @endif
         <ul>
             <!-- SEARCH START -->
           {{--  <li>
@@ -275,6 +302,48 @@
             });
 
         });
+
+        var $worked = $("#active-timer");
+        var activeBreak = "{{ (!is_null($selfActiveTimer) && !is_null($selfActiveTimer->activeBreak)) }}";
+
+        function updateTimerTask() {
+            var myTime = $worked.html();
+            var ss = myTime.split(":");
+
+            var hours = ss[0];
+            var mins = ss[1];
+            var secs = ss[2];
+            secs = parseInt(secs) + 1;
+
+            if (secs > 59) {
+                secs = '00';
+                mins = parseInt(mins) + 1;
+            }
+
+            if (mins > 59) {
+                secs = '00';
+                mins = '00';
+                hours = parseInt(hours) + 1;
+            }
+
+            if (hours.toString().length < 2) {
+                hours = '0' + hours;
+            }
+            if (mins.toString().length < 2) {
+                mins = '0' + mins;
+            }
+            if (secs.toString().length < 2) {
+                secs = '0' + secs;
+            }
+            var ts = hours + ':' + mins + ':' + secs;
+
+            $worked.html(ts);
+            setTimeout(updateTimerTask, 1000);
+        }
+
+        if ($('#active-timer').length && activeBreak != '1') {
+            setTimeout(updateTimerTask, 1000);
+        }
 
     });
 </script>
