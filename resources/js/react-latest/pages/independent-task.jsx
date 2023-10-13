98809d5ee23c110b1af3ba77fe43b7c04ad73eb1
nafis-seopage1 <nafis@seopage1.net>
2023-10-13 17:49:35 +0600

    task table column modified

--- resources/js/react-latest/pages/independent-task.jsx
+++ resources/js/react-latest/pages/independent-task.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import IndependentTaskDataTable from '../components/independent-task-page/IndependentTaskDataTable';
 import data from '../components/independent-task-page/tasks/components/demo.json';
+import { useGetIndependentTaskQuery } from '../services/api/independentTaskApiSlice';
 
 const IndependentTask = () => {
   // console.log(data);
+  const {data:tasks} = useGetIndependentTaskQuery();
+  console.log(tasks);
 
   return (
     <div>
