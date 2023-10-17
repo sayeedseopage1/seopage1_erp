44731baa2a91885ee6a64c2cfa220024ad4de9ea
sayeedseopage1 <113491743+sayeedseopage1@users.noreply.github.com>
2023-10-17 16:42:26 +0600

    npm run dev

--- app/Traits/PmDashboardAdminView.php
+++ app/Traits/PmDashboardAdminView.php
@@ -2968,16 +2968,35 @@ trait PmDashboardAdminView
 
           $this->no_of_delayed_projects = Project::select('projects.*','p_m_projects.delayed_status as delayed_status', 'p_m_projects.created_at as project_creation_date')
           ->leftJoin('p_m_projects', 'p_m_projects.project_id', '=', 'projects.id')
-          ->where('projects.pm_id', $this->pm->id)
+         
+          ->where(function ($q3) use( $startMonth, $endMonth,$release_date) {
+
+            $q3->whereBetween('projects.updated_at', [$startMonth, $release_date])
+               ->whereBetween('pm_projects.created_at', [$startMonth, $endMonth])
+               ->where('projects.pm_id', $this->pm->id)
+               ->where('projects.project_status', 'Accepted')
+               
+               ->where('p_m_projects.delayed_status', 1)
+              
+               ;
+                           
+        })
+      ->orWhere(function ($q2) use( $startMonth,$release_date,$nextMonth){
+               $q2->whereBetween('projects.updated_at', [$nextMonth, $release_date])
+                ->where('pm_projects.created_at', '<', $startMonth)
+                ->where('projects.pm_id', $this->pm->id)
           ->where('projects.project_status', 'Accepted')
           
           ->where('p_m_projects.delayed_status', 1)
-          ->whereNotBetween('p_m_projects.created_at', [$endMonth, $release_date])
-          ->whereBetween('projects.updated_at', [$startMonth, $release_date])
+             
+                ;
+  
+    })
           ->orderBy('projects.updated_at','desc')
 
 
           ->get();
+          
 
          // dd($delay_date);
 
