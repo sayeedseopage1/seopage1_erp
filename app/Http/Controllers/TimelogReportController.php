de4bf1021420ca274e68ee579ec0f3974dab984c
sayeedseopage1 <113491743+sayeedseopage1@users.noreply.github.com>
2023-11-13 16:47:13 +0600

    pending_action

--- app/Http/Controllers/TimelogReportController.php
+++ app/Http/Controllers/TimelogReportController.php
@@ -582,6 +582,7 @@ class TimelogReportController extends AccountBaseController
         }
         
         $data = $filteredData->groupBy('project_time_logs.user_id')->get();
+        dd($data);
        
         
         
@@ -604,6 +605,22 @@ class TimelogReportController extends AccountBaseController
             // Move to the next day
             $currentDate->addDay();
         }
+        foreach ($data as $item) {
+            $total_minutes_progress = ProjectTimelog::
+                where('user_id', $item->employee_id)
+                ->where('start_time', '!=', null)
+                ->where('end_time', null)
+                ->selectRaw('CAST(TIME_TO_SEC(TIMEDIFF(NOW(), start_time)) / 60 AS SIGNED) AS total_minutes_progress')
+                ->first();
+            
+            if ($total_minutes_progress) {
+                $item->total_minutes = $item->total_minutes + $total_minutes_progress->total_minutes_progress;
+              
+            } else {
+                $item->total_minutes = $item->total_minutes;
+               
+            }
+        }
         
         // Convert the result into a collection
         $data = collect($data);
@@ -621,22 +638,7 @@ class TimelogReportController extends AccountBaseController
           
         });
     //   /  dd($data);
-    foreach ($data as $item) {
-        $total_minutes_progress = ProjectTimelog::
-            where('user_id', $item->employee_id)
-            ->where('start_time', '!=', null)
-            ->where('end_time', null)
-            ->selectRaw('CAST(TIME_TO_SEC(TIMEDIFF(NOW(), start_time)) / 60 AS SIGNED) AS total_minutes_progress')
-            ->first();
-        
-        if ($total_minutes_progress) {
-            $item->total_minutes = $item->total_minutes + $total_minutes_progress->total_minutes_progress;
-          
-        } else {
-            $item->total_minutes = $item->total_minutes;
-           
-        }
-    }
+    
        
         //$data->missed_hours_count = $missedNumber;
         return response()->json([
