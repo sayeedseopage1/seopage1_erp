810751b5fc7dbd7cd7ebc9e36c57ec21347617ff
sayeedseopage1 <113491743+sayeedseopage1@users.noreply.github.com>
2023-11-21 19:25:53 +0600

    task_revisions

--- resources/js/react/tasks/components/PrimaryPageAuthorizationTable.jsx
+++ resources/js/react/tasks/components/PrimaryPageAuthorizationTable.jsx
@@ -203,7 +203,7 @@ export const Columns = [
     },
     {
         id: "task",
-        header: "Task",
+        header: "Sub Task",
         accessorKey: 'task',
         draggable: true,
         cell: ({ row }) => {
@@ -220,25 +220,25 @@ export const Columns = [
             );
         },
     },
-    {
-        id: "sub_task",
-        header: "SubTask",
-        accessorKey: 'sub_task',
-        draggable: true,
-        cell: ({ row }) => {
-            const data = row.original;
-            return (
-                <abbr title={data?.sub_task}>
-                    <a
-                        href={`/account/tasks/${data?.sub_task_id}`}
-                        className="multiline-ellipsis"
-                    >
-                        {data?.sub_task}
-                    </a>
-                </abbr>
-            );
-        },
-    },
+    // {
+    //     id: "sub_task",
+    //     header: "SubTask",
+    //     accessorKey: 'sub_task',
+    //     draggable: true,
+    //     cell: ({ row }) => {
+    //         const data = row.original;
+    //         return (
+    //             <abbr title={data?.sub_task}>
+    //                 <a
+    //                     href={`/account/tasks/${data?.sub_task_id}`}
+    //                     className="multiline-ellipsis"
+    //                 >
+    //                     {data?.sub_task}
+    //                 </a>
+    //             </abbr>
+    //         );
+    //     },
+    // },
 
     {
         id: "assigned_by",
