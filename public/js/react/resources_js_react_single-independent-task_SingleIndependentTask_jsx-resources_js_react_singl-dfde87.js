1ab6302753689308b513df19324acc4b03fff242
mehedihasanseopage1 <130024264+mehedihasanseopage1@users.noreply.github.com>
2023-11-25 13:17:33 +0600

    'update_revision'

--- public/js/react/resources_js_react_single-independent-task_SingleIndependentTask_jsx-resources_js_react_singl-dfde87.js
+++ public/js/react/resources_js_react_single-independent-task_SingleIndependentTask_jsx-resources_js_react_singl-dfde87.js
@@ -155,7 +155,7 @@ var useRevision = function useRevision() {
       }, {
         id: 'LDRx4',
         revision: "The instruction was followed but the developer missed out on some default/basic things or best practices which are not essential to mention in instruction.",
-        isDeniable: false
+        isDeniable: true
       }];
     }
   };
