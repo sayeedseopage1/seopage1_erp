ae5080e985fec0a3e63dbcb330bee7b38e014581
mehedihasanseopage1 <130024264+mehedihasanseopage1@users.noreply.github.com>
2023-12-07 18:56:53 +0600

    'update'

--- resources/js/react/single-task/components/StartTimerWorkingReportPopup.jsx
+++ resources/js/react/single-task/components/StartTimerWorkingReportPopup.jsx
@@ -4,9 +4,12 @@ import styled from 'styled-components';
 export const StartTimerWorkingReport = () => {
     return(
        <div>
-            <Title>You haven't submitted the last day's working report.</Title>
+            {/* <Title>You haven't submitted the last day's working report.</Title>
             <Text> Do you want to submit the working report?</Text>
             <Note><strong>Note:</strong> If you didn't submit last day's working report, you won't be able to start the timer</Note>
+            */}
+            <Title>You haven't clocked in today.</Title>
+            <Text> Do you want to clock in now?</Text>  
        </div>
     )
 }
