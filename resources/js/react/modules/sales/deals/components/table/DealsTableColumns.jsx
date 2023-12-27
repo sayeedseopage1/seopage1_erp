8e61f3519a86cee7af9493c2950820fcf5b39e2b
mehedihasanseopage1 <130024264+mehedihasanseopage1@users.noreply.github.com>
2023-12-27 17:15:03 +0600

    'update'

--- resources/js/react/modules/sales/deals/components/table/DealsTableColumns.jsx
+++ resources/js/react/modules/sales/deals/components/table/DealsTableColumns.jsx
@@ -1,20 +1,20 @@
-// import dayjs from "dayjs";
-// import {
-//     Action,
-//     BidValue,
-//     BiddingDelayTime,
-//     CreatedAt,
-//     CreatedBy,
-//     DealStatus,
-//     ProjectBudget,
-//     ProjectID,
-//     Status,
-// } from "./ui";
+import dayjs from "dayjs";
+import {
+    Action,
+    BidValue,
+    BiddingDelayTime,
+    CreatedAt,
+    CreatedBy,
+    DealStatus,
+    EmptySpace,
+    ProjectBudget,
+    ProjectID,
+    Status,
+} from "./ui";
 // import { Menu } from "@headlessui/react";
 // import ActionDropdown from "./ActionDropdown";
-// import Avatar from "../../../../../global/Avatar";
-
-import { EmptySpace } from "./ui";
+import Avatar from "../../../../../global/Avatar";
+ 
 
 export const DealsTableColumns = [
     {
@@ -70,12 +70,12 @@ export const DealsTableColumns = [
     {
         id: "project_budget",
         header: "Project Budget (USD)",
-        accessorKey: "bid_value2",
+        accessorKey: "amount",
         cell: ({ row }) => {
             const data = row.original;
             return (
-                <ProjectBudget>
-                    {/* {`${data?.currency_symbol}${data?.bid_value} - ${data?.currency_symbol}${data?.bid_value2}`} */}
+                <ProjectBudget> 
+                    {`${data?.ammount_currency_symbol} ${data?.amount}`}
                 </ProjectBudget>
             );
         },
@@ -83,12 +83,12 @@ export const DealsTableColumns = [
     {
         id: "project_budget_original_currency",
         header: "Project Budget (Original Currency)",
-        accessorKey: "bid_value2",
+        accessorKey: "actual_amount",
         cell: ({ row }) => {
             const data = row.original;
             return (
                 <ProjectBudget>
-                    {/* {`${data?.currency_symbol}${data?.bid_value} - ${data?.currency_symbol}${data?.bid_value2}`} */}
+                    {`${data?.actual_amount_currency_symbol} ${data?.actual_amount}`}
                 </ProjectBudget>
             );
         },
@@ -104,106 +104,76 @@ export const DealsTableColumns = [
                 : <EmptySpace> -- </EmptySpace>;
             return <CreatedAt>{date}</CreatedAt>;
         },
-    },
- 
-   
+    }, 
     {
         id: "added_by",
         header: "Added By",
-        accessorKey: "add",
+        accessorKey: "lead_added_by",
         cell: ({ row }) => {
             const data = row.original;
             return (
                 <CreatedBy
-                    href={`/account/employees/${data.added_by}`}
+                    href={`/account/employees/${data.lead_added_by}`}
                 >
                     <Avatar
                         type="circle"
-                        name={data?.agent_name}
-                        src={data?.user?.image_url ?? null}
+                        name={data?.lead_added_by_name}
+                        src={data?.lead_added_by_image ? `/user-uploads/avatar/${data?.lead_added_by_image}` : null}
                     />
 
-                    <span>{data?.agent_name}</span>
+                    <span>{data?.lead_added_by_name}</span>
                 </CreatedBy>
             );
         },
     },
-    // {
-    //     id: "bidding_delay_time",
-    //     header: "Bidding Delay Time",
-    //     accessorFn: (row) => row?.bidding_minutes * 60 + row?.bidding_seconds,
-    //     cell: ({ row }) => {
-    //         const data = row.original;
-    //         return (
-    //             <BiddingDelayTime>
-    //                 {`${data?.bidding_minutes} min ${data?.bidding_seconds} sec`}
-    //             </BiddingDelayTime>
-    //         );
-    //     },
-    // },
-    // {
-    //     id: "status",
-    //     header: "Status",
-    //     cell: ({ row }) => {
-    //         const data = row.original;
+    {
+        id: "closed_by",
+        header: "Closed By",
+        accessorKey: "lead_added_by",
+        cell: ({ row }) => {
+            const data = row.original;
+            return (
+                <CreatedBy
+                    href={`/account/employees/${data.deal_stages_converted_by}`}
+                >
+                    <Avatar
+                        type="circle"
+                        name={data?.deal_stages_converted_by_name}
+                        src={data?.deal_stages_converted_by_image ? `/user-uploads/avatar/${data?.deal_stages_converted_by_image}` : null}
+                    />
 
-    //         return (
-    //             <Status>
-    //                 {data.deal_status === 0 ? (
-    //                     <span className="badge badge-danger">
-    //                         {" "}
-    //                         Not Converted to Deal{" "}
-    //                     </span>
-    //                 ) : (
-    //                     <span className="badge badge-success">
-    //                         {" "}
-    //                         Converted to Deal{" "}
-    //                     </span>
-    //                 )}
-    //             </Status>
-    //         );
-    //     },
-    // },
-    // {
-    //     id: "deal_status",
-    //     header: "Deal Status",
-    //     cell: ({ row }) => {
-    //         const data = row.original;
-    //         const status = [
-    //             {
-    //                 label: "Not Applicable",
-    //                 bgColor: "transparent",
-    //                 color: "#777",
-    //             },
-    //             {
-    //                 label: "Won",
-    //                 bgColor: "#0AAA29",
-    //                 color: "#fff",
-    //             },
-    //             {
-    //                 label: "Lost",
-    //                 bgColor: "#EC3003",
-    //                 color: "#fff",
-    //             },
-    //             {
-    //                 label: "Not Activity Yet",
-    //                 bgColor: "#FAA700",
-    //                 color: "#fff",
-    //             },
-    //         ];
+                    <span>{data?.deal_stages_converted_by_name}</span>
+                </CreatedBy>
+            );
+        },
+    },
+    {
+        id: "status",
+        header: "Status",
+        cell: ({ row }) => {
+            const data = row.original;
 
-    //         const s = status[data?.won_lost];
-
-    //         return (
-    //             <DealStatus backgroundColor={s.bgColor} color={s.color}>
-    //                 {s.label}
-    //             </DealStatus>
-    //         );
-    //     },
-    // },
-    // {
-    //     id: "action",
-    //     header: "Action",
-    //     cell: (props) => <ActionDropdown {...props} />,
-    // },
+            return (
+                <Status>
+                    {data.deal_status === 0 ? (
+                        <span className="badge badge-danger">
+                            {" "}
+                            Not Converted to Deal{" "}
+                        </span>
+                    ) : (
+                        <span className="badge badge-success">
+                            {" "}
+                            Converted to Deal{" "}
+                        </span>
+                    )}
+                </Status>
+            );
+        },
+    },
+    
+    {
+        id: "action",
+        header: "Action",
+        cell: (props) => '',
+    },
 ];
