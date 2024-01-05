d135084c74ca1f929883672db2a6fc84fb71b49f
sayeedseopage1 <113491743+sayeedseopage1@users.noreply.github.com>
2024-01-05 12:25:35 +0600

    contract_bug_fixed

--- resources/views/contracts/editdealdetails.blade.php
+++ resources/views/contracts/editdealdetails.blade.php
@@ -357,6 +357,25 @@
                                         <span id="deal_category_error" class="text-danger"></span>
                                     </div>
                                 </div>
+                              @if($deal->cms_id == null)
+                               <div class="col-md-6" id="cmsSelect" style="display: none">
+                            <label class="f-14 text-dark-grey mb-12" data-label="true" for="project_cms">Project CMS
+                                <sup class="f-14 mr-1">*</sup>
+                                <svg class="svg-inline--fa fa-question-circle fa-w-16" data-toggle="popover" data-placement="top" data-content="Enter the project cms name." data-html="true" data-trigger="hover" aria-hidden="true" focusable="false" data-prefix="fa" data-icon="question-circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg="" data-original-title="" title="">
+                                    <path fill="currentColor" d="M504 256c0 136.997-111.043 248-248 248S8 392.997 8 256C8 119.083 119.043 8 256 8s248 111.083 248 248zM262.655 90c-54.497 0-89.255 22.957-116.549 63.758-3.536 5.286-2.353 12.415 2.715 16.258l34.699 26.31c5.205 3.947 12.621 3.008 16.665-2.122 17.864-22.658 30.113-35.797 57.303-35.797 20.429 0 45.698 13.148 45.698 32.958 0 14.976-12.363 22.667-32.534 33.976C247.128 238.528 216 254.941 216 296v4c0 6.627 5.373 12 12 12h56c6.627 0 12-5.373 12-12v-1.333c0-28.462 83.186-29.647 83.186-106.667 0-58.002-60.165-102-116.531-102zM256 338c-25.365 0-46 20.635-46 46 0 25.364 20.635 46 46 46s46-20.636 46-46c0-25.365-20.635-46-46-46z"></path>
+                                </svg>
+                            </label>
+                            <div class="dropdown bootstrap-select form-control select-picker">
+                                <select name="cms_id" id="cms_id" data-live-search="true" class="form-control select-picker error" data-size="8">
+                                    <option value="">--</option>
+                                    @foreach ($project_cms as $item)
+                                        <option value="{{ $item->id .'-'. $item->cms_name }}">{{ $item->cms_name }}</option>
+                                    @endforeach
+                                </select>
+                               
+                            </div>
+                        </div>
+                              @endif
                                 @if($deal->cms_id && $deal->cms_name)
                                 <div class="col-md-6">
                                     <label class="f-14 text-dark-grey mb-12" data-label="true" for="project_cms">Project CMS
@@ -1295,6 +1314,19 @@
                  });
              });
          </script>
+@if($deal->cms_id == null)
+<script>
+        var dealCategorySelect = document.getElementById("deal_category");
+        var cmsSection = document.querySelector("#cmsSelect");
+        dealCategorySelect.addEventListener("change", function() {
+            if (dealCategorySelect.value === "Web Development") {
+                cmsSection.style.display = "block";
+            } else {
+                cmsSection.style.display = "none";
+            }
+        });
+    </script>
+@endif
     <script>
         flatpickr("#deadline", {
             dateFormat: "Y-m-d",
