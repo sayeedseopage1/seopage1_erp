aa94a62a235f1edfd8350b0d34fed6c9d5b1b3a8
sayeedseopage1 <113491743+sayeedseopage1@users.noreply.github.com>
2024-02-23 19:02:02 +0600

    fix_bug

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -1211,7 +1211,7 @@ class TaskController extends AccountBaseController
                 ->count();
                
             // dd($gene/ralRevisionCount) ;
-            if($generalRevisionCount >= 2){
+            if($generalRevisionCount >= 1){
                 return response()->json([
                     'error' => true,
                     'message' => 'You have already attempted <span class="badge badge-danger">General Revision</span> maximum time'
