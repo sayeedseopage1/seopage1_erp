6baaf4401a0f59b9e56d40c59074fbcf6bb4832b
sayeedseopage1 <113491743+sayeedseopage1@users.noreply.github.com>
2024-02-27 15:27:18 +0600

    fix_bug

--- resources/js/react/Points/components/TypeFilter.jsx
+++ resources/js/react/Points/components/TypeFilter.jsx
@@ -1,12 +1,9 @@
 import * as React from "react";
-import { Listbox } from "@headlessui/react";
-import styles from "./PersonFilter.module.css";
-import { filter, lowerCase, includes } from "lodash";
-import { GoTriangleDown } from "react-icons/go"; 
 import Dropdown from './Dropdown';
 import Search from './Searchbox';
+import { filter, lowerCase, includes } from "lodash";
 
-export default function TypeFilter({ value, onChange, data ,sidebarIsOpen, selectionBoxClassName, disabled }) {
+export default function TypeFilter({ value, onChange, data ,sidebarIsOpen, disabled, selectionBoxClassName}) {
     const [query, setQuery] = React.useState("");
 
     const filteredData = data
@@ -16,96 +13,37 @@ export default function TypeFilter({ value, onChange, data ,sidebarIsOpen, selec
               )
             : data
         : [];
-    return (
-        <>
-            {/* <div className={`${styles.toggleWrapper} ${sidebarIsOpen && "w-100"}`}>
-                <span>Type: </span>
 
-                <Listbox
-                    as="div"
-                    value={value}
-                    onChange={onChange}
-                    className={styles.dropdown}
-                >
-                    <Listbox.Button className={styles.dropdownToggle}>
-                        <span
-                            className={`${styles.dropdownToggleButtonText} singleline-ellipsis`}
-                        >
-                            {value?.title ?? <span>All</span>}
-                        </span>
-                        <GoTriangleDown className={styles.dropdownIcon} />
-                    </Listbox.Button>
-                    <Listbox.Options className={styles.dropdownMenu}>
-                        <div className={styles.searchBox}>
-                            <i className="fa-solid fa-search" />
-                            <input
-                                type="text"
-                                placeholder="Search..."
-                                value={query}
-                                onKeyDown={e => e.stopPropagation()}
-                                onChange={(e) => setQuery(e.target.value)}
-                            />
-                        </div>
-                        <div className={styles.options}>
-                            <Listbox.Option
-                                className={({ active, selected }) =>
-                                    `${styles.dropdownItem} ${
-                                        active && styles.dropdownItemActive
-                                    } ${selected && styles.dropdownItemSelected}`
-                                }
-                                value={null}
-                            >
-                                All
-                            </Listbox.Option>
-                            {filteredData?.map((item) => (
-                                <Listbox.Option
-                                    className={({ active, selected }) =>
-                                        `${styles.dropdownItem} ${
-                                            active && styles.dropdownItemActive
-                                        } ${
-                                            selected && styles.dropdownItemSelected
-                                        }`
-                                    }
-                                    key={item.id}
-                                    value={item}
-                                >
-                                    {item.title}
-                                </Listbox.Option>
-                            ))}
-                        </div>
-                    </Listbox.Options>
-                </Listbox>
-            </div> */}
-            <div className={`sp1_task_filter_item d-flex ${sidebarIsOpen && "flex-column w-100"}`}>
-            <span className='mr-2 f-13 d-flex flex-nowrap'>Type: :</span>
+    return (
+        <div className={`sp1_task_filter_item d-flex ${sidebarIsOpen && "flex-column"}`}>
+            <span className='mr-2 f-13 d-flex flex-nowrap'>Type: </span>
             <Dropdown>
-                <Dropdown.Toggle disabled={disabled} className={`sp1_filter_toggle ${selectionBoxClassName ?? ''} ${sidebarIsOpen && "py-2"}`} >
-                    <span
-                        data-toggle={name ? 'tooltip' : ''}
-                        title={value?.title ?? ''}
-                    >
-                        <strong>{name ?? 'All'}</strong>
-                    </span>
-                </Dropdown.Toggle>
+                <Dropdown.Toggle disabled={disabled} className={`sp1_filter_toggle ${selectionBoxClassName ?? ''} ${sidebarIsOpen && "py-2"} `}>
+                        <span
+                            data-toggle={name ? 'tooltip' : ''}
+                            title={value?.title ?? ''}
+                        >
+                            <strong>{value?.title ?? 'All'}</strong>
+                        </span>
+                    </Dropdown.Toggle>
+            
                 <Dropdown.Menu >
-                    <div>
-                        <Search autoFocus={true} value={query} onChange={setQuery} />
-                    </div>
-                    <div className="sp1_filter--users">
-                        {value?.title &&
-                            <Dropdown.Item onClick={() => onChange(null)} className={value?.title === null ? 'sp1_filter--user active' : 'sp1_filter--user'}>
-                               {value?.title ?? <span>All</span>}
-                            </Dropdown.Item>}
-                         {filteredData?.map((item) => (
-                                <Dropdown.Item key={value?.id} onClick={() => onChange(item)} className={value?.id === item.id ? 'sp1_filter--user active' : 'sp1_filter--user'}
-                                >
-                                    {item.title}
+                        <div>
+                            <Search autoFocus={true} value={query} onChange={setQuery} />
+                        </div>
+                        <div className="sp1_filter--users">
+                            <Dropdown.Item onClick={() => onChange(null)} className={value === null ? 'sp1_filter--user active' : 'sp1_filter--user'}>
+                                    All
+                            </Dropdown.Item>
+                            {_.map(filteredData, item => {
+                                return <Dropdown.Item key={item.id} onClick={() => onChange(item)} className={value?.id === item.id ? 'sp1_filter--user active' : 'sp1_filter--user'}>
+                                    
+                                    {item?.title}
                                 </Dropdown.Item>
-                        ))}
-                    </div>
+                            })}
+                        </div>
                 </Dropdown.Menu>
             </Dropdown>
-            </div>
-        </>
+        </div>
     );
 }
