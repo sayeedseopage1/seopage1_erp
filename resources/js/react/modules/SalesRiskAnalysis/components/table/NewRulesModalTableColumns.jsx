aaf7a9c8a3251035ddf036d2b369dc9437ced42b
mahfujseopage1 <mahfujar@seopage1.net>
2024-03-11 17:50:34 +0600

    new policy form done

--- resources/js/react/modules/SalesRiskAnalysis/components/table/NewRulesModalTableColumns.jsx
+++ resources/js/react/modules/SalesRiskAnalysis/components/table/NewRulesModalTableColumns.jsx
@@ -8,9 +8,9 @@ const validPolicyTypes = ["lessThan", "greaterThan", "fixed", "range"];
 
 export const NewRulesModalTableColumnsData = [
     {
-        id: "policy_rules",
-        header: "Policy Rules",
-        accessorKey: "policy_rules",
+        id: "rules_name",
+        header: "Rules Name",
+        accessorKey: "rules_name",
         cell: ({ row }) => {
             const data = row?.original;
             console.log("table", data);
@@ -83,12 +83,170 @@ export const NewRulesModalTableColumnsData = [
                                     }}
                                     className="py-3 pr-2"
                                 >
-                                    {data.title} 
+                                    {data.title}
                                 </p>
                                 <MultiSelectShowDropDown
                                     data={data?.countries}
                                     multiple
                                 />
+                            </div>
+                        </Switch.Case>
+                    </Switch>
+                </div>
+            );
+        },
+    },
+    {
+        id: "rules_type",
+        header: "Rules Type",
+        accessorKey: "rules_type",
+        cell: ({ row }) => {
+            const data = row?.original;
+            console.log("table", data);
+            return (
+                <div className="d-flex justify-content-start align-items-start flex-column">
+                    <Switch.Case condition={data?.policyType.name !== "yes/No"}>
+                        <p
+                            style={{
+                                color: "#8F8F8F",
+                                fontSize: "14px",
+                                fontFamily: "Poppins",
+                            }}
+                            className="py-3"
+                        >
+                            {data?.rulesType?.label}
+                        </p>
+                    </Switch.Case>
+                    <Switch.Case condition={data?.policyType.name === "yes/No"}>
+                        <p
+                            style={{
+                                color: "#8F8F8F",
+                                fontSize: "14px",
+                                fontFamily: "Poppins",
+                            }}
+                            className="py-3"
+                        >   </p>
+                        <ul
+                            style={{
+                                listStyle: "disc",
+                                padding: "0 1.5rem",
+                            }}
+                        >
+                            <li
+                                style={{
+                                    listStyle: "disc",
+                                    padding: "4px 0",
+                                }}
+                            >
+                                Not Applicable
+                            </li>
+                            <li
+                                style={{
+                                    listStyle: "disc",
+                                    padding: "4px 0",
+                                }}
+                            >
+                                Not Applicable
+                            </li>
+                        </ul>
+                    </Switch.Case>
+                </div>
+            );
+        },
+    },
+    {
+        id: "value",
+        header: "Value",
+        accessorKey: "value",
+        cell: ({ row }) => {
+            const data = row?.original;
+
+            return (
+                <div className="d-flex justify-content-start align-items-start flex-column">
+                    <Switch>
+                        <Switch.Case
+                            condition={_.includes(
+                                ["lessThan", "greaterThan", "fixed"],
+                                data?.policyType.name
+                            )}
+                        >
+                            <p
+                                style={{
+                                    color: "#8F8F8F",
+                                    fontSize: "14px",
+                                    fontFamily: "Poppins",
+                                }}
+                                className="py-3"
+                            >
+                                {data.value}
+                            </p>
+                        </Switch.Case>
+                        <Switch.Case
+                            condition={_.includes(
+                                ["range"],
+                                data?.policyType.name
+                            )}
+                        >
+                            <p
+                                style={{
+                                    color: "#8F8F8F",
+                                    fontSize: "14px",
+                                    fontFamily: "Poppins",
+                                }}
+                                className="py-3"
+                            >
+                                {data.from}-{data.to}
+                            </p>
+                        </Switch.Case>
+                        <Switch.Case
+                            condition={data?.policyType.name === "yes/No"}
+                        >
+                            <p
+                                style={{
+                                    color: "#8F8F8F",
+                                    fontSize: "14px",
+                                    fontFamily: "Poppins",
+                                }}
+                                className="py-3"
+                            ></p>
+                            <ul
+                                style={{
+                                    listStyle: "disc",
+                                    padding: "0 1.5rem",
+                                }}
+                            >
+                                <li
+                                    style={{
+                                        listStyle: "disc",
+                                        padding: "4px 0",
+                                    }}
+                                >
+                                    Not Applicable
+                                </li>
+                                <li
+                                    style={{
+                                        listStyle: "disc",
+                                        padding: "4px 0",
+                                    }}
+                                >
+                                    Not Applicable
+                                </li>
+                            </ul>
+                        </Switch.Case>
+                        <Switch.Case
+                            condition={data?.policyType.name === "list"}
+                        >
+                            <div className="d-flex">
+                                <p
+                                    style={{
+                                        color: "#8F8F8F",
+                                        fontSize: "14px",
+                                        fontFamily: "Poppins",
+                                    }}
+                                    className="py-3 pr-2"
+                                >
+                                    Not Applicable
+                                </p>
                             </div>
                         </Switch.Case>
                     </Switch>
