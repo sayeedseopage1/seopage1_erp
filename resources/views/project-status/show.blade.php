b96901d58b43ddc96bf0037203385197c1177aef
mdrayhanseopage1 <rayhan@seopage1.net>
2024-03-12 15:31:54 +0600

    pm_goal_calendar_view

--- resources/views/project-status/show.blade.php
+++ resources/views/project-status/show.blade.php
@@ -1,5 +1,6 @@
 @php
     $pmGoal = App\Models\ProjectPmGoal::where('id',$project_status)->first();
+    $project = App\Models\Project::where('id',$pmGoal->project_id)->first();
 @endphp
 <div id="holiday-detail-section">
     <div class="row">
@@ -38,6 +39,11 @@
                     </div>
                 </div>
                 <div class="card-body">
+                    <div class="col-12 px-0 pb-3 d-lg-flex d-md-flex d-block">
+                        <p class="mb-0 text-lightest f-14 w-30 text-capitalize">Project Name</p>
+                        <div class="mb-0 text-dark-grey f-14 w-70 text-wrap p-0"><a href="{{route('projects.show',$project->id)}}">{{$project->project_name}}</a></div>
+                    </div>
+                    <x-cards.data-row :label="__('Goal Name')" :value="$pmGoal->goal_name" html="true" />
                     <x-cards.data-row :label="__('Goal Start Date')" :value="$pmGoal->goal_start_date" html="true" />
                     <x-cards.data-row :label="__('Goal Deadline')" :value="$pmGoal->goal_end_date" html="true" />
                     <x-cards.data-row :label="__('Duration')" :value="$pmGoal->duration . ' Days'" html="true" />
