73cf3bfc77df6513571b91a811d14451eb44e671
Saidul Islam <saidul@seopage1.net>
2024-03-14 10:31:37 +0600

    Change draft message of own deal

--- resources/views/contracts/dealdetails.blade.php
+++ resources/views/contracts/dealdetails.blade.php
@@ -695,7 +695,7 @@
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" value="1" name="is_drafted" id="is_drafted">
                                     <label class="form-check-label m-1" for="flexCheckDefault">
-                                        Want to make it as draft
+                                        Keep it as draft
                                     </label>
                                 </div>
                               </div>
