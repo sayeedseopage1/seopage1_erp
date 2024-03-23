0d48242a807e17a64e19b990040d6e6b9a2a1290
tanvirmitul1 <mitulseopage1@gmail.com>
2024-03-23 12:48:48 +0600

    api fixed

--- resources/js/react/services/api/EvaluationApiSlice.js
+++ resources/js/react/services/api/EvaluationApiSlice.js
@@ -24,7 +24,7 @@ const evaluationApiSlice = apiSlice.injectEndpoints({
             invalidatesTags: ["All_TASKS", "All_EVALUATION"],
         }),
         getEvaluationList: build.query({
-            query: () => `http://localhost:3000/api/evaluation//get-evaluation`,
+            query: () => `http://localhost:3000/api/evaluation/get-evaluation`,
             providesTags: ["All_EVALUATION"],
         }),
     }),
