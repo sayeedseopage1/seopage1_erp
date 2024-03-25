8486a93e9a486297b9d529c586a8aeabf9e9eef1
Saidul Islam <saidul@seopage1.net>
2024-03-25 14:09:38 +0600

    add fields and defalt entries to factors

--- database/migrations/2024_03_25_092939_create_criterias_table.php
+++ database/migrations/2024_03_25_092939_create_criterias_table.php
@@ -19,6 +19,65 @@ return new class extends Migration
             $table->string('title');
             $table->timestamps();
         });
+
+        \Illuminate\Support\Facades\DB::table('criterias')->insert([
+                [
+                    'id' => 1,
+                    'title' => 'Signing the deliverables'
+                ],
+                [
+                    'id' => 2,
+                    'title' => 'Estimated hours vs logged hours'
+                ],
+                [
+                    'id' => 3,
+                    'title' => 'Amount of revisions'
+                ],
+                [
+                    'id' => 4,
+                    'title' => 'Milestone release'
+                ],
+                [
+                    'id' => 5,
+                    'title' => 'Project completion'
+                ],
+                [
+                    'id' => 6,
+                    'title' => 'Creating the tasks'
+                ],
+                [
+                    'id' => 7,
+                    'title' => 'Meeting the deadline'
+                ],
+                [
+                    'id' => 8,
+                    'title' => 'Reviewing the work'
+                ],
+                [
+                    'id' => 9,
+                    'title' => 'Number of projects completed in 7 days'
+                ],
+                [
+                    'id' => 10,
+                    'title' => 'Number of projects completed in 12 days'
+                ],
+                [
+                    'id' => 11,
+                    'title' => 'If the first submission is made within 100 hours of the form fill up time, the project manager will get some points depending on the project budget'
+                ],
+                [
+                    'id' => 12,
+                    'title' => 'Task hold time'
+                ],
+                [
+                    'id' => 13,
+                    'title' => 'PM goals'
+                ],
+                [
+                    'id' => 14,
+                    'title' => "For every revision due to Pm's own fault (where in revision calculator, he is held responsible. Partial ones will also count)"
+                ]
+            ]);
     }
 
     /**
--- database/migrations/2024_03_25_094026_create_factors_table.php
+++ database/migrations/2024_03_25_094026_create_factors_table.php
@@ -17,13 +17,122 @@ return new class extends Migration
             $table->id();
             $table->foreignId('criteria_id')->constrained();
             $table->string('title');
-            
-            $table->unsignedTinyInteger('point_type')->default(1)->comment('1 = Static, 2 = Percentage');
+            $table->unsignedTinyInteger('project_type')->comment('1 = Fixed, 2 = Hourly');
+            $table->decimal('lower_limit')->nullable();
+            $table->decimal('upper_limit')->nullable();
+            $table->unsignedTinyInteger('limit_type')->nullable()->comment('1 = Static, 2 = Percentage');
+            $table->unsignedTinyInteger('limit_unit')->nullable()->comment('1 = hour, 2 = day');
+            $table->string('lower_limit_condition')->nullable()->comment('>,<,=,=>,=<,!=');
+            $table->string('upper_limit_condition')->nullable()->comment('>,<,=,=>,=<,!=');
+            $table->string('limit_depend_on_models_and_fields')->nullable()->comment('The limit depend on this model and fields');
+            $table->unsignedTinyInteger('point_type')->nullable()->comment('1 = Static, 2 = Percentage');
             $table->decimal('points');
             $table->string('point_depend_on_model')->nullable()->comment('The point depend on this model');
             $table->string('point_depend_on_field')->nullable()->comment('The point depend on this field of defined model');
+            $table->unsignedTinyInteger('status')->default(1)->comment('1 = Active, 0 = Deactive');
             $table->timestamps();
         });
+
+        \Illuminate\Support\Facades\DB::table('factors')->insert([
+            [
+                'criteria_id' => 1,
+                'title' => 'Deliverables are signed by the client within 48 hours after the entry of the project in the system',
+                'project_type' => 1,
+                'lower_limit' => 0,
+                'upper_limit' => 48,
+                'limit_type' => 1,
+                'limit_unit' => 1,
+                'lower_limit_condition' => '<',
+                'upper_limit_condition' => '=>',
+                'limit_depend_on_models_and_fields' => json_encode([['model' => 'App\Models\Deal::class', 'field' => 'released_at'], ['model' => 'App\Models\ContractSign::class', 'field' => 'created_at']]),
+                'point_type' => 1,
+                'points' => 5
+            ],
+            [
+                'criteria_id' => 2,
+                'title' => 'Estimated hours matching the logged hours by 81%-120%',
+                'project_type' => 1,
+                'lower_limit' => 80,
+                'upper_limit' => 120,
+                'limit_type' => 2,
+                'limit_unit' => 1,
+                'lower_limit_condition' => '<',
+                'upper_limit_condition' => '=>',
+                'limit_depend_on_models' => json_encode([['model' => 'App\Models\Task::class', 'field' => 'estimate_hours'], ['model' => 'App\Models\ProjectTimeLog::class', 'field' => 'total_hours']]),
+                'point_type' => 1,
+                'points' => 5
+            ],
+            [
+                'criteria_id' => 2,
+                'title' => 'Estimated hours matching the logged hours by 71%-80% or 121%-130%',
+                'project_type' => 1,
+                'lower_limit' => 70,
+                'upper_limit' => 130,
+                'limit_type' => 2,
+                'limit_unit' => 1,
+                'lower_limit_condition' => '<',
+                'upper_limit_condition' => '=>',
+                'limit_depend_on_models' => json_encode([['model' => 'App\Models\Task::class', 'field' => 'estimate_hours'], ['model' => 'App\Models\ProjectTimeLog::class', 'field' => 'total_hours']]),
+                'point_type' => 1,
+                'points' => 4
+            ],
+            [
+                'criteria_id' => 2,
+                'title' => 'Estimated hours matching the logged hours by 61%-70% or 131%-140%',
+                'project_type' => 1,
+                'lower_limit' => 60,
+                'upper_limit' => 140,
+                'limit_type' => 2,
+                'limit_unit' => 1,
+                'lower_limit_condition' => '<',
+                'upper_limit_condition' => '=>',
+                'limit_depend_on_models' => json_encode([['model' => 'App\Models\Task::class', 'field' => 'estimate_hours'], ['model' => 'App\Models\ProjectTimeLog::class', 'field' => 'total_hours']]),
+                'point_type' => 1,
+                'points' => 3
+            ],
+            [
+                'criteria_id' => 2,
+                'title' => 'Estimated hours matching the logged hours by 56%-60% or 141%-145%',
+                'project_type' => 1,
+                'lower_limit' => 55,
+                'upper_limit' => 145,
+                'limit_type' => 2,
+                'limit_unit' => 1,
+                'lower_limit_condition' => '<',
+                'upper_limit_condition' => '=>',
+                'limit_depend_on_models' => json_encode([['model' => 'App\Models\Task::class', 'field' => 'estimate_hours'], ['model' => 'App\Models\ProjectTimeLog::class', 'field' => 'total_hours']]),
+                'point_type' => 1,
+                'points' => 2
+            ],
+            [
+                'criteria_id' => 2,
+                'title' => 'Estimated hours matching the logged hours by 51%-55% or 146%-150%',
+                'project_type' => 1,
+                'lower_limit' => 50,
+                'upper_limit' => 150,
+                'limit_type' => 2,
+                'limit_unit' => 1,
+                'lower_limit_condition' => '<',
+                'upper_limit_condition' => '=>',
+                'limit_depend_on_models' => json_encode([['model' => 'App\Models\Task::class', 'field' => 'estimate_hours'], ['model' => 'App\Models\ProjectTimeLog::class', 'field' => 'total_hours']]),
+                'point_type' => 1,
+                'points' => 1
+            ],
+            [
+                'criteria_id' => 2,
+                'title' => 'Estimated hours matching the logged hours by >50% or <150%',
+                'project_type' => 1,
+                'lower_limit' => 50,
+                'upper_limit' => 150,
+                'limit_type' => 2,
+                'limit_unit' => 1,
+                'lower_limit_condition' => '>',
+                'upper_limit_condition' => '=<',
+                'limit_depend_on_models' => json_encode([['model' => 'App\Models\Task::class', 'field' => 'estimate_hours'], ['model' => 'App\Models\ProjectTimeLog::class', 'field' => 'total_hours']]),
+                'point_type' => 1,
+                'points' => -5
+            ],
+        ]);
     }
 
     /**
