53ed4c782b47d79c91ff2725b8a9cec1f4975335
moonseopage1 <moon@seopage1.net>
2024-04-04 12:06:46 +0600

    text opacity adjust by status

--- resources/js/react/modules/Pm-Sales/PointFactors/components/table/PointFactorsColumns.jsx
+++ resources/js/react/modules/Pm-Sales/PointFactors/components/table/PointFactorsColumns.jsx
@@ -51,14 +51,16 @@ export const PointFactorsColumns = [
                 <div className="d-flex flex-column justify-content-start align-items-start">
                     {
                         _.map(data?.factors, (factor) => {
+                            const textStyle = {
+                                color: "#000",
+                                opacity: factor?.status == 0 ? ".25" : "1",
+                                fontSize: "14px",
+                                fontFamily: "Poppins",
+                            };
                             return (
                                 <p
                                     key={factor?.id}
-                                    style={{
-                                        color: "#000",
-                                        fontSize: "14px",
-                                        fontFamily: "Poppins",
-                                    }}
+                                    style={textStyle}
                                     className="py-3"
                                 >
                                     {factor?.title}
@@ -81,24 +83,29 @@ export const PointFactorsColumns = [
                 <div className="d-flex justify-content-end flex-column align-items-end">
                     {
                         _.map(data?.factors, (factor) => {
+                            const textStyle = {
+                                opacity: factor?.status == 0 ? ".25" : "1",
+                            };
                             return (
                                 <SalesPointsContainer
                                     key={factor?.id}
                                     className="py-3"
                                 >
                                     <div className="d-flex align-items-center justify-content-end">
-                                        <Switch key={factor?.id}>
-                                            <Switch.Case condition={factor?.point_type === 1}>
-                                                <p className={`${factor?.points < 0 && 'text-danger'}`}>
-                                                    {factor?.points} {(factor?.points == 1 || factor?.points == -1 || factor?.points == 0) ? 'Point' : 'Points'}
-                                                </p>
-                                            </Switch.Case>
-                                            <Switch.Case condition={factor?.point_type === 2}>
-                                                <p className={`${factor?.points < 0 && 'text-danger'}`}>
-                                                    {factor?.points}% of the project budget
-                                                </p>
-                                            </Switch.Case>
-                                        </Switch>
+                                        <div style={textStyle}>
+                                            <Switch key={factor?.id}>
+                                                <Switch.Case condition={factor?.point_type === 1}>
+                                                    <p className={`${factor?.points < 0 && 'text-danger'}`}>
+                                                        {factor?.points} {(factor?.points == 1 || factor?.points == -1 || factor?.points == 0) ? 'Point' : 'Points'}
+                                                    </p>
+                                                </Switch.Case>
+                                                <Switch.Case condition={factor?.point_type === 2}>
+                                                    <p className={`${factor?.points < 0 && 'text-danger'}`}>
+                                                        {factor?.points}% of the project budget
+                                                    </p>
+                                                </Switch.Case>
+                                            </Switch>
+                                        </div>
 
                                         <div
                                             onClick={() => {
