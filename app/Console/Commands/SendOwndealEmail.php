619a32793f1fda9fe197249abfb6de9460026d64
Saidul Islam <saidul@seopage1.net>
2024-04-07 16:47:32 +0600

    The send-owndealemail command created and updated

---
+++ app/Console/Commands/SendOwndealEmail.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Notification;
+use App\Models\Deal;
+use App\Models\User;
+use Illuminate\Console\Command;
+use App\Notifications\WonDealNotification;
+use App\Notifications\HourlyDealNotification;
+use App\Notifications\DealAuthorizationSendNotification;
+
+class SendOwndealEmail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'send-owndeal-email';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'If team lead does not authorize the owndeal then send require email based on cirtain conditions';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $deals = Deal::where('authorization_status')->get();
+
+        foreach($deals as $deal){
+            $user = User::where('id', $deal->pm_id)->first();
+            if ($deal->project_type == 'fixed') {
+                Notification::send($user, new WonDealNotification($deal));
+            }else{
+                Notification::send($user, new HourlyDealNotification($deal));
+            }
+    
+            if ($deal->project_type == 'fixed') {
+                $users = User::where('role_id', 1)->get();
+                foreach ($users as $usr) {
+                    Notification::send($usr, new WonDealNotification($deal));
+                }
+            }else{
+                $users = User::where('role_id', 1)->get();
+                foreach ($users as $usr) {
+                    Notification::send($usr, new HourlyDealNotification($deal));
+                }
+            }
+    
+            // $users = User::where('role_id', 8)->get();
+    
+            // foreach ($users as $key => $user) {
+            //     Notification::send($user, new DealAuthorizationSendNotification($deal, Auth::user()));
+            // }
+        }
+        
+    }
+}
