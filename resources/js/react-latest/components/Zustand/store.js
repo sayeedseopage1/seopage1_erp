8b6f6cc489608751ea2ba2435ed5f84b36667cbf
tanvirmitul1 <mitulseopage1@gmail.com>
2024-04-18 17:12:26 +0600

    single evaluation state zustand add

--- resources/js/react-latest/components/Zustand/store.js
+++ resources/js/react-latest/components/Zustand/store.js
@@ -1,11 +1,18 @@
 import { create } from "zustand";
 
-// Define your Zustand store
 const useCounterStore = create((set) => ({
     count: 0,
 
     increaseCount: () => {
         set((state) => ({ count: state.count + 1 }));
+    },
+}));
+
+export const usePendingActionStore = create((set) => ({
+    pendingAction: {},
+    setPendingAction: (obj) => {
+        set({ pendingAction: obj });
+        localStorage.setItem("selectedPendingAction", JSON.stringify(obj));
     },
 }));
 
--- resources/js/react/zustand/store.js
+++ resources/js/react/zustand/store.js
@@ -3,7 +3,6 @@ import { create } from "zustand";
 const useEmployeeEvaluation = create((set) => ({
     evaluationObject: {},
     setEvaluationObject: (object) => {
-        console.log("Setting evaluation object:", object);
         set({ evaluationObject: object });
     },
 }));
