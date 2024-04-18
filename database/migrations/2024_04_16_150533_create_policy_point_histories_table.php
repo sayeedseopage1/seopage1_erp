5e87dc84938e1a02e468d02306d06832aaed8c0d
Tonoy <tonoy@seopage1.net>
2024-04-18 17:31:03 +0600

    small update

--- database/migrations/2024_04_16_150533_create_policy_point_histories_table.php
+++ database/migrations/2024_04_16_150533_create_policy_point_histories_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->bigInteger('deal_id');
             $table->json('policy');
+            $table->json('point_report');
             $table->timestamps();
         });
     }
