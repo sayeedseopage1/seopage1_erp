e4efb6f8d8ef05dc27dca4f9cadce4331e7974e4
Saidul Islam <saidul@seopage1.net>
2024-04-22 08:37:14 +0600

    Modify a deals migration

--- database/migrations/2024_04_19_160433_add_email_send_status_to_deals_table.php
+++ database/migrations/2024_04_19_160433_add_email_send_status_to_deals_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Deal;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,6 +17,8 @@ return new class extends Migration
         Schema::table('deals', function (Blueprint $table) {
             $table->unsignedTinyInteger('email_send_status')->nullable()->after('released_at');
         });
+
+        Deal::query()->update(['email_send_status' => 1]);
     }
 
     /**
