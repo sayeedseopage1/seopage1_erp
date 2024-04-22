dfef0e9a83c0d312a1386f09d0d5dc811c21189e
moonseopage1 <moon@seopage1.net>
2024-04-22 16:45:19 +0600

    [FIX] avatar issues on task page and project task page

--- resources/js/react/global/PersonAvatar.jsx
+++ resources/js/react/global/PersonAvatar.jsx
@@ -1,43 +1,48 @@
 import React from 'react'
 
-const PersonAvatar = ({ avatar, name }) => {
+const PersonAvatar = ({ avatar, name, url }) => {
     return (
-        <div className='' style={{ width: '32px' }}>
-            {avatar ?
-                <div
-                    style={{
-                        width: "32px",
-                        height: "32px",
-                    }}
-                >
-                    <img
-                        src={`/user-uploads/avatar/${avatar}`}
-                        alt={name}
-                        width="32px"
-                        height="32px"
-                        className="rounded-circle"
-                        onError={(e) => {
-                            e.target.src = "/img/avatar.png";
-                            e.target.alt = name?.slice(0, 1).toUpperCase() || 'A'; // 'A' is the default if name is undefined
-                        }}
-                    />
-                </div>
-                : <div
-                    className="sp1-item-center border rounded-circle"
-                    style={{
-                        width: "32px",
-                        height: "32px",
-                    }}
-                >
+        <div className='d-flex align-items-center'>
+            <div className='' style={{ width: '32px' }}>
+                {avatar ?
                     <div
                         style={{
-                            fontSize: "1rem",
-                            fontWeight: "bold",
+                            width: "32px",
+                            height: "32px",
                         }}
                     >
-                        {name?.slice(0, 1).toUpperCase()}
+                        <img
+                            src={`/user-uploads/avatar/${avatar}`}
+                            alt={name}
+                            width="32px"
+                            height="32px"
+                            className="rounded-circle"
+                            onError={(e) => {
+                                e.target.src = "/img/avatar.png";
+                                e.target.alt = name?.slice(0, 1).toUpperCase() || 'A'; // 'A' is the default if name is undefined
+                            }}
+                        />
                     </div>
-                </div>
+                    : <div
+                        className="sp1-item-center border rounded-circle"
+                        style={{
+                            width: "32px",
+                            height: "32px",
+                        }}
+                    >
+                        <div
+                            style={{
+                                fontSize: "1rem",
+                                fontWeight: "bold",
+                            }}
+                        >
+                            {name?.slice(0, 1).toUpperCase()}
+                        </div>
+                    </div>
+                }
+            </div>
+            {
+                url && <a href={url} className='pl-2 '>{name}</a>
             }
         </div>
     )
