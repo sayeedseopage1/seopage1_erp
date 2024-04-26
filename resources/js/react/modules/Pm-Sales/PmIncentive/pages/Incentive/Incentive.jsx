46e5678c7d14b9e3765d03bce19e02b04c36310f
moonseopage1 <moon@seopage1.net>
2024-04-26 12:42:25 +0600

    [ADD] setup top filter section initially, make this functionality later

--- resources/js/react/modules/Pm-Sales/PmIncentive/pages/Incentive/Incentive.jsx
+++ resources/js/react/modules/Pm-Sales/PmIncentive/pages/Incentive/Incentive.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import IncentiveFilter from '../../components/IncentiveFilter';
 
 const Incentive = () => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [isDataFetching, setIsDataFetching] = useState(true);
+
+
+
+    React.useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoading(false)
+        }, 1000)
+
+        return () => clearTimeout(timer)
+    }, [])
+
     return (
         <div>
-            Hello Incentive Page
+            <IncentiveFilter setData={setData}
+                setIsDataFetching={setIsDataFetching}
+            />
         </div>
     );
 };
