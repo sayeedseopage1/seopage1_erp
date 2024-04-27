288e59d413a65af222f5c6300658c79873ccd72f
Saidul Islam <saidul@seopage1.net>
2024-04-27 13:20:43 +0600

    Production build for staging

--- public/js/sp1-editor.js
+++ public/js/sp1-editor.js
@@ -1,18 +1,1 @@
-(self["webpackChunk"] = self["webpackChunk"] || []).push([["/js/sp1-editor"],{
-
-/***/ "./resources/js/sp1-editor.js":
-/*!************************************!*\
-  !*** ./resources/js/sp1-editor.js ***!
-  \************************************/
-/***/ (() => {
-
-
-
-/***/ })
-
-},
-/******/ __webpack_require__ => { // webpackRuntimeModules
-/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-/******/ var __webpack_exports__ = (__webpack_exec__("./resources/js/sp1-editor.js"));
-/******/ }
-]);
+(self.webpackChunk=self.webpackChunk||[]).push([[242],{45302:()=>{}},e=>{var k;k=45302,e(e.s=k)}]);
