e0a3e310e31c59482711f49096da1ea2e404af5e
moonseopage1 <moon@seopage1.net>
2024-04-27 08:32:18 +0600

    [WIP] incentive page layout and stats save work

--- resources/js/react/modules/Pm-Sales/PmIncentive/components/StatsInfo.jsx
+++ resources/js/react/modules/Pm-Sales/PmIncentive/components/StatsInfo.jsx
@@ -4,9 +4,17 @@ import StatsInfoProgressCard from './StatsInfoProgressCard';
 const StatsInfo = () => {
     return (
         <div className='stats_info_wrapper'>
+            {/* point stats */}
             <div className='stats_info_outer'>
                 <StatsInfoProgressCard />
+                <StatsInfoProgressCard />
+                <StatsInfoProgressCard />
+                <StatsInfoProgressCard />
+                <StatsInfoProgressCard />
+                <StatsInfoProgressCard />
+                <StatsInfoProgressCard />
             </div>
+            {/* point score */}
             <div className='stats_score_outer'>
                 stats_score_outer
             </div>
