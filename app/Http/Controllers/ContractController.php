c85a2d9006b862ed8ffe5a7147a27533efb55669
Tonoy <tonoy@seopage1.net>
2024-04-29 11:31:39 +0600

    won deal list function updated

--- app/Http/Controllers/ContractController.php
+++ app/Http/Controllers/ContractController.php
@@ -3024,7 +3024,7 @@ public function getAllContracts(Request $request){
     ->leftJoin('users as client', 'client.id', 'deals.client_id')
     ->leftJoin('p_m_projects', 'deals.id', 'p_m_projects.deal_id')
     ->where('deals.dept_status','WD')
-    ->whereIn('deals.sale_analysis_status',['privious-won','privious-denied', 'authorized', 'auto-authorized']);
+    ->whereNotIn('deals.sale_analysis_status',['denied', 'analysis', 'pending']);
 
     if ($startDate !== null && $endDate !== null) {
         $dealsQuery->where(function ($q) use ($startDate, $endDate) {
