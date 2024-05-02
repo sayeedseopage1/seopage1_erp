f868d1a81da89b163bafe6b6aba4c7e88bef2893
Saidul Islam <saidul@seopage1.net>
2024-05-02 11:53:11 +0600

    Pm goal logic implemented for PM Point

--- app/Models/ProjectPmGoal.php
+++ app/Models/ProjectPmGoal.php
@@ -2,10 +2,25 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Helper\ProjectManagerPointLogic;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ProjectPmGoal extends Model
 {
     use HasFactory;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::updated(function ($item) {
+            if ($item->isDirty('goal_status') && $item->goal_status == 0) {
+                $comparableValue = $item->project_category == 'Regular' ? 1 : ($item->project_category == 'Priority' ? 2 : ($item->project_category == 'High-priority' ? 3 : ($item->project_category == 'Top most priority' ? 4 : ($item->project_category == 'Critically sensitive' ? 5 : 0))));
+            
+                // Project Manager Point Distribution ( PM goals )
+                ProjectManagerPointLogic::distribute(15, $item->project_id, $comparableValue);
+            }
+        });
+    }
 }
