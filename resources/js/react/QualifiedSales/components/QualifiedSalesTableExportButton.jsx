f799553d377b95081f1524e94183ba47710ce8ed
tanvirmitul1 <mitulseopage1@gmail.com>
2024-05-03 14:13:44 +0600

    qualified sales export button

---
+++ resources/js/react/QualifiedSales/components/QualifiedSalesTableExportButton.jsx
@@ -0,0 +1,315 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import dayjs from "dayjs";
+import ReactExport from "react-data-export";
+import _, { fill } from "lodash";
+import styled from "styled-components";
+
+import Loader from "../../global/Loader";
+import { useExportableQualifiedSalesMutation } from "../../services/api/qualifiedSalesApiSlice";
+
+import { CompareDate } from "../../Insights/utils/dateController";
+const ExcelFile = ReactExport.ExcelFile;
+const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
+
+const QualifiedSalesTableExportButton = ({ filter }) => {
+    const dayjs2 = new CompareDate();
+    const [isRender, setIsRender] = React.useState(false);
+    const queryObject = _.pickBy(filter ?? {}, Boolean);
+    const query = new URLSearchParams(queryObject).toString();
+
+    const [getQualifiedSales, { data, isFetching, isLoading }] =
+        useExportableQualifiedSalesMutation();
+
+    const qualifiedSales = data;
+
+    console.log("qualified sales", qualifiedSales);
+
+    // get deals
+    const getData = (deals) => {
+        let rows = [];
+
+        _.forEach(deals, (d) => {
+            const fieldStyle = {
+                alignment: {
+                    // wrapText: true,
+                    vertical: "center",
+                    horizontal: "top",
+                },
+                font: {
+                    bold: d?.project_type?.toLowerCase() === "hourly",
+                    color: {
+                        rgb:
+                            d?.project_type?.toLowerCase() === "hourly"
+                                ? "FF28A745"
+                                : "",
+                    },
+                },
+            };
+
+            // const clientContactForm = (d) => {
+            //     if (d?.toLowerCase() === "submitted") {
+            //         return {
+            //             fill: {
+            //                 fgColor: { rgb: "FF28A745" },
+            //             },
+            //             font: {
+            //                 color: { rgb: "FFFFFFFF" },
+            //             },
+            //         };
+            //     } else {
+            //         return {
+            //             fill: {
+            //                 fgColor: { rgb: "FFFCBD01" },
+            //             },
+            //             font: {
+            //                 color: { rgb: "FF000000" },
+            //             },
+            //         };
+            //     }
+            // };
+
+            // status
+            const statusStyle = (row) => {
+                let isPMAccepted = Number(row?.accepted_by_project_manager);
+
+                let isTopMApproved = Number(row?.authorized_by_admin);
+                let isSLApproved = Number(row?.authorized_by_sales_lead);
+                let date1 = dayjs2.dayjs(row?.date).format("YYYY-MM-DD");
+                let curr = dayjs2.dayjs().format("YYYY-MM-DD");
+                let diff = dayjs2.dayjs(curr).diff(date1, "day") + 1;
+
+                if (isPMAccepted && isTopMApproved && isSLApproved) {
+                    return {
+                        fill: {
+                            fgColor: { rgb: "00AA00" },
+                        },
+                        font: {
+                            color: { rgb: "FFFFFFFF" },
+                        },
+                    };
+                } else if (!isPMAccepted || !isTopMApproved || !isSLApproved) {
+                    // any of two accept
+                    if (diff > 5) {
+                        return {
+                            fill: {
+                                fgColor: { rgb: "FCBD01" },
+                            },
+                            font: {
+                                color: { rgb: "FFFFFFFF" },
+                            },
+                        };
+                    }
+
+                    return {
+                        fill: {
+                            fgColor: { rgb: "1D82F5" },
+                        },
+                        font: {
+                            color: { rgb: "FFFFFFFF" },
+                        },
+                    };
+                }
+            };
+
+            const Status = (row) => {
+                let isPMAccepted = Number(row?.accepted_by_project_manager);
+
+                let isTopMApproved = Number(row?.authorized_by_admin);
+                let isSLApproved = Number(row?.authorized_by_sales_lead);
+                let date1 = dayjs2.dayjs(row?.date).format("YYYY-MM-DD");
+                let curr = dayjs2.dayjs().format("YYYY-MM-DD");
+                let diff = dayjs2.dayjs(curr).diff(date1, "day") + 1;
+
+                if (isPMAccepted && isTopMApproved && isSLApproved) {
+                    // all accept
+                    return " Qualified";
+                } else if (!isPMAccepted || !isTopMApproved || !isSLApproved) {
+                    // any of two accept
+                    if (diff > 5) {
+                        return "On Hold";
+                    }
+
+                    return "In Progress";
+                }
+            };
+
+            // date
+            const date = (_data) =>
+                _data ? dayjs(_data).format(`DD-MM-YYYY hh:mm:ss A`) : "--";
+
+            let row = [
+                {
+                    value: d["date"] ?? "--",
+                    style: fieldStyle,
+                },
+                {
+                    value:
+                        `${d["project_name"]} ${
+                            d?.project_type?.toLowerCase() === "hourly"
+                                ? " ( Hourly )"
+                                : ""
+                        }` ?? "--",
+                    style: {
+                        ...fieldStyle,
+                    },
+                },
+                {
+                    value: d["client_name"] ?? "--",
+                    style: fieldStyle,
+                },
+                {
+                    value: d["amount"] ?? "--",
+                    style: fieldStyle,
+                },
+                {
+                    value: d["closed_by_name"] ?? "--",
+                    style: fieldStyle,
+                },
+                {
+                    value: d["pm_name"] ?? "--",
+                    style: fieldStyle,
+                },
+                {
+                    value: d?.["deal_id"] ?? "--",
+                    style: fieldStyle,
+                },
+                {
+                    value: d?.authorized_by_sales_lead,
+                },
+
+                {
+                    value: date(d?.accepted_by_project_manager) ?? "--",
+                    style: fieldStyle,
+                },
+                {
+                    value: d?.["authorized_by_admin"] ?? "--",
+                    style: fieldStyle,
+                },
+                {
+                    value: Status(d) ?? "--",
+
+                    style: {
+                        ...fieldStyle,
+                        ...statusStyle(d),
+                    },
+                },
+                {
+                    value: d?.["total_points"] ?? "--",
+
+                    style: {
+                        ...fieldStyle,
+                        // ...statusStyle(d?.total_points),
+                    },
+                },
+            ];
+            rows.push(row);
+        });
+
+        return rows;
+    };
+
+    // columns
+    const columns = [
+        { title: "Date" },
+        { title: "Project Name" },
+        { title: "Client Name" },
+        { title: "Amount" },
+        { title: "Closed By" },
+        { title: "Project Manager" },
+        { title: "Contact Form" },
+        { title: "Authorized By Sales Lead" },
+        { title: "Accepted by Project Manager" },
+        { title: "Authorized By Top Management" },
+        { title: "Status" },
+        { title: "points Earned" },
+    ];
+
+    // multi data set
+    const multiDataSet = [
+        {
+            columns: [
+                { title: "Filter" },
+                { title: "Date" },
+                { title: "Project Manager" },
+                { title: "Client" },
+                { title: "Closed By" },
+                { title: "Status" },
+            ],
+            data: [
+                [
+                    {
+                        value: `--`,
+                    },
+                    {
+                        value: `${dayjs(filter?.start_date).format(
+                            "MMM-DD-YYYY"
+                        )} to ${dayjs(filter?.end_date).format("MMM-DD-YYYY")}`,
+                        style: {
+                            font: {
+                                bold: true,
+                            },
+                        },
+                    },
+                    { value: filter?.pm_name ?? "--" },
+                    { value: filter?.client_username ?? "--" },
+                    { value: filter?.closed_by_name ?? "--" },
+                    { value: filter?.status_title ?? "--" },
+                ],
+            ],
+        },
+        {
+            xSteps: 0,
+            ySteps: 2,
+            columns: columns,
+            data: getData(qualifiedSales),
+        },
+    ];
+
+    const handleRender = async () => {
+        setIsRender(false);
+        await getQualifiedSales(query).unwrap();
+        setIsRender(true);
+    };
+
+    return (
+        <React.Fragment>
+            <ExportButton onClick={handleRender}>
+                {false ? (
+                    <>
+                        <Loader title="Processing..." />
+                    </>
+                ) : (
+                    <>
+                        <i className="fa-solid fa-download" /> Export To Excel
+                    </>
+                )}
+            </ExportButton>
+
+            {isRender && !isLoading && qualifiedSales?.length > 0 && (
+                <ExcelFile filename="qualified_sales_table" hideElement={true}>
+                    <ExcelSheet
+                        dataSet={multiDataSet}
+                        name="qualified sales table"
+                    />
+                </ExcelFile>
+            )}
+        </React.Fragment>
+    );
+};
+
+export default QualifiedSalesTableExportButton;
+
+const ExportButton = styled.button`
+    margin-left: 10px;
+    margin-top: 10px;
+    width: 140px;
+    padding: 6px 10px;
+    border-radius: 3px;
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    /* color: #DAF7A6; */
+    color: #000;
+    background-color: #c4de95;
+`;
