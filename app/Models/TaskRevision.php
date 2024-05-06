17e6b2eb681de7b0566579bb0225e1392f8e023b
Saidul Islam <saidul@seopage1.net>
2024-05-06 13:25:28 +0600

    Revision due to PM faults criteria implementatin started

--- app/Models/TaskRevision.php
+++ app/Models/TaskRevision.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class TaskRevision extends Model
 {
     use HasFactory;
+
+    protected static function boot(){
+        parent::boot();
+
+        static::created(function($item){
+            if($item->final_responsible_person && $item->final_responsible_person == 'PM'){
+                // 
+            }elseif(in_array($item->dispute_between, ['PLR', 'CPR', 'SPR']) && ($item->raised_by_percent || $item->raised_against_percent)){
+                // 
+            }
+        });
+
+        static::updated(function($item){
+            if($item->isDirty('final_responsible_person') && $item->final_responsible_person && $item->final_responsible_person == 'PM'){
+                // 
+            }elseif(($item->isDirty('raised_by_percent') || $item->isDirty('raised_against_percent')) && in_array($item->dispute_between, ['PLR', 'CPR', 'SPR']) && ($item->raised_by_percent || $item->raised_against_percent)){
+                // 
+            }
+        });
+    }
 }
