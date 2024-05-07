01d8028873d1ef86a05febf44b5cb19ce7f5f43c
Saidul Islam <saidul@seopage1.net>
2024-05-07 10:33:55 +0600

    Revision due to PM faults criteria modified

--- app/Models/TaskRevision.php
+++ app/Models/TaskRevision.php
@@ -15,7 +15,7 @@ class TaskRevision extends Model
 
         static::created(function($item){
             if(($item->final_responsible_person && $item->final_responsible_person == 'PM') || (in_array($item->dispute_between, ['PLR', 'CPR', 'SPR']) && ($item->raised_by_percent || $item->raised_against_percent))){
-                $project_category = ProjectPmGoal::where('project_id', $item->project_id)->first()->project_category;
+                $project_category = ProjectPmGoal::where('project_id', $item->project_id)->first()->project_category ?? null;
                 $comparableValue = $project_category == 'Regular' ? 1 : ($project_category == 'Priority' ? 2 : ($project_category == 'High-priority' ? 3 : ($project_category == 'Top most priority' ? 4 : ($project_category == 'Critically sensitive' ? 5 : 0))));
                 
                 // Project Manager Point Distribution ( For every revision due to Pm's own fault )
@@ -25,7 +25,7 @@ class TaskRevision extends Model
 
         static::updated(function($item){
             if(($item->isDirty('final_responsible_person') && $item->final_responsible_person && $item->final_responsible_person == 'PM') || (($item->isDirty('raised_by_percent') || $item->isDirty('raised_against_percent')) && in_array($item->dispute_between, ['PLR', 'CPR', 'SPR']) && ($item->raised_by_percent || $item->raised_against_percent))){
-                $project_category = ProjectPmGoal::where('project_id', $item->project_id)->first()->project_category;
+                $project_category = ProjectPmGoal::where('project_id', $item->project_id)->first()->project_category ?? null;
                 $comparableValue = $project_category == 'Regular' ? 1 : ($project_category == 'Priority' ? 2 : ($project_category == 'High-priority' ? 3 : ($project_category == 'Top most priority' ? 4 : ($project_category == 'Critically sensitive' ? 5 : 0))));
                 
                 // Project Manager Point Distribution ( For every revision due to Pm's own fault )
