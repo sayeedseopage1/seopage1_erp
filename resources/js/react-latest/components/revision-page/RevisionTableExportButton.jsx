a546e33901b05a65c6b356dddde28258ad5c8b09
tanvirmitul1 <mitulseopage1@gmail.com>
2024-05-08 17:20:48 +0600

    revision table export add

--- resources/js/react-latest/components/revision-page/RevisionTableExportButton.jsx
+++ resources/js/react-latest/components/revision-page/RevisionTableExportButton.jsx
@@ -7,9 +7,8 @@ import styled from "styled-components";
 
 import Loader from "../../../react/global/Loader";
 import { CompareDate } from "../../../react/Insights/utils/dateController";
-import { useExportableQualifiedSalesMutation } from "../../../react/services/api/qualifiedSalesApiSlice";
+
 import { useExportTableRevisionMutation } from "../../services/api/revisionApiSlice";
-import Switch from "../../ui/Switch";
 
 const ExcelFile = ReactExport.ExcelFile;
 const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
@@ -20,7 +19,7 @@ const RevisionTableExportButton = ({ filter }) => {
     const queryObject = _.pickBy(filter ?? {}, Boolean);
     const query = new URLSearchParams(queryObject).toString();
 
-    const [getRevisions, { data, isFetching, isLoading }] =
+    const [getRevisions, { data, isLoading }] =
         useExportTableRevisionMutation();
 
     const Revisions = data?.data;
@@ -46,74 +45,35 @@ const RevisionTableExportButton = ({ filter }) => {
                 },
             };
 
-            const statusStyle = (row) => {
-                let isPMAccepted = Number(row?.accepted_by_project_manager);
-                let isTopMApproved = Number(row?.authorized_by_admin);
-                let isSLApproved = Number(row?.authorized_by_sales_lead);
-                let date1 = dayjs2.dayjs(row?.date).format("YYYY-MM-DD");
-                let curr = dayjs2.dayjs().format("YYYY-MM-DD");
-                let diff = dayjs2.dayjs(curr).diff(date1, "day") + 1;
-
-                if (isPMAccepted && isTopMApproved && isSLApproved) {
-                    return {
-                        fill: {
-                            fgColor: { rgb: "00AA00" },
-                        },
-                        font: {
-                            color: { rgb: "FFFFFFFF" },
-                        },
-                    };
-                } else if (!isPMAccepted || !isTopMApproved || !isSLApproved) {
-                    if (diff > 5) {
-                        return {
-                            fill: {
-                                fgColor: { rgb: "FCBD01" },
-                            },
-                            font: {
-                                color: { rgb: "FFFFFFFF" },
-                            },
-                        };
-                    }
-
-                    return {
-                        fill: {
-                            fgColor: { rgb: "1D82F5" },
-                        },
-                        font: {
-                            color: { rgb: "FFFFFFFF" },
-                        },
-                    };
-                }
-            };
-
             const Status = (row) => {
                 const data = row;
                 const user = window?.Laravel?.user;
 
                 const actionAlreadyTaken =
-                    data.dispute_between === "SPR"
-                        ? data.sale_accept || data.sale_deny
-                        : data.is_accept || data.is_deny;
+                    data?.dispute_between === "SPR"
+                        ? data?.sale_accept || data?.sale_deny
+                        : data?.is_accept || data?.is_deny;
 
                 const hasPermissionToTakeAction =
-                    Number(user.id) === Number(data?.deal_added_by.id);
-
-                if (!hasPermissionToTakeAction || actionAlreadyTaken) {
+                    Number(user?.id) === Number(data?.deal_added_by?.id);
+                if (data?.approval_status === "pending") {
+                    return "Pending";
+                } else if (!hasPermissionToTakeAction || actionAlreadyTaken) {
                     return `Accepted By ${data?.deal_added_by?.name}`;
-                } else if (data.dispute_between === "SPR" && data.sale_deny) {
+                } else if (data?.dispute_between === "SPR" && data?.sale_deny) {
                     return `Denied By ${data?.deal_added_by?.name}`;
-                } else if (data.dispute_between !== "SPR" && data.is_accept) {
+                } else if (data?.dispute_between !== "SPR" && data?.is_accept) {
                     return `Accepted by ${data?.task_assign_to?.name}`;
-                } else if (data.dispute_between !== "SPR" && data.is_accept) {
+                } else if (data?.dispute_between !== "SPR" && data?.is_accept) {
                     return `Accepted by ${data?.task_assign_to?.name}`;
                 } else if (
-                    data.dispute_between !== "SPR"
+                    data?.dispute_between !== "SPR"
                         ? data?.approval_status === "pending"
                         : !actionAlreadyTaken
                 ) {
                     return "Pending";
                 } else if (
-                    data.dispute_between === "SPR" &&
+                    data?.dispute_between === "SPR" &&
                     !actionAlreadyTaken &&
                     hasPermissionToTakeAction
                 ) {
@@ -121,6 +81,27 @@ const RevisionTableExportButton = ({ filter }) => {
                 }
             };
 
+            const statusStyle = (data) => {
+                if (data?.approval_status === "pending") {
+                    return {
+                        fill: {
+                            fgColor: { rgb: "fada25" },
+                        },
+                        font: {
+                            color: { rgb: "FFFFFFFF" },
+                        },
+                    };
+                } else {
+                    return {
+                        fill: {
+                            fgColor: { rgb: "49db2c" },
+                        },
+                        font: {
+                            color: { rgb: "FFFFFFFF" },
+                        },
+                    };
+                }
+            };
             const date = (_data) =>
                 _data ? dayjs(_data).format(`DD-MM-YYYY hh:mm:ss A`) : "--";
 
@@ -190,10 +171,11 @@ const RevisionTableExportButton = ({ filter }) => {
                     },
                 },
                 {
-                    value: Status(d) ?? "--",
+                    value: Status(d),
 
                     style: {
                         ...fieldStyle,
+                        ...statusStyle(d),
                     },
                 },
             ];
@@ -272,7 +254,7 @@ const RevisionTableExportButton = ({ filter }) => {
     return (
         <React.Fragment>
             <ExportButton onClick={handleRender}>
-                {false ? (
+                {isLoading ? (
                     <>
                         <Loader title="Processing..." />
                     </>
