a8577bd33d7fb2e36ea59d294830e8cddcdf7e61
Tonoy <tonoy@seopage1.net>
2024-05-10 15:56:35 +0600

    upsell start date update

--- app/Http/Controllers/HelperPmProjectStatusController.php
+++ app/Http/Controllers/HelperPmProjectStatusController.php
@@ -1128,4 +1128,44 @@ class HelperPmProjectStatusController extends AccountBaseController
             DB::rollBack();
         }
     }
+
+    public function upsellGoalCreation(ProjectMilestone $milestone)
+    {
+        // get total milestone amount
+        $milestoneSum = ProjectMilestone::where('project_id', $milestone->project_id)->sum('cost');
+        $project = Project::find($milestone->project_id);
+
+        // check if total budget has been completed
+        // if not then pm goal will not be created
+        // dd($milestoneSum, $project->project_budget, $milestoneSum < $project->project_budget);
+        if ($milestoneSum < $project->project_budget) {
+            return;
+        }
+
+        $lastGoal = ProjectPmGoal::where('project_id', $milestone->project_id)->orderBy('goal_end_date', 'desc')->first();
+
+        $goalCodes = ProjectPmGoal::$goalCodes['upsell'];
+
+        // dd($milestone);
+
+        if($milestone->cost < 100) $goalDuration = 3;
+        else $goalDuration = 3 +  (int) (($milestone->cost -100) / 100);
+
+        $goal = new ProjectPmGoal();
+        $goal->project_id = $lastGoal->project_id;
+        $goal->client_id = $lastGoal->client_id;
+        $goal->pm_id = $lastGoal->pm_id;
+        $goal->project_type = $lastGoal->project_type;
+        $goal->project_category = $lastGoal->project_category;
+
+        $goal->goal_code = $goalCodes['code'];
+        $goal->goal_name = $goalCodes['name'];
+        $goal->goal_type = $goalCodes['type'];
+
+        $goal->goal_start_date = $lastGoal->goal_start_date;
+        $goal->goal_end_date = Carbon::parse($lastGoal->goal_start_date)->addDay($lastGoal->duration + $goalDuration);
+        $goal->duration = $lastGoal->duration + $goalDuration;
+        $goal->added_by = Auth::user()->id;
+        $goal->save();
+    }
 }
--- app/Listeners/MilestoneAddedEventListener.php
+++ app/Listeners/MilestoneAddedEventListener.php
@@ -3,6 +3,7 @@
 namespace App\Listeners;
 
 use App\Events\MilestoneAddedEvent;
+use App\Http\Controllers\HelperPmProjectStatusController;
 use App\Models\Deal;
 use App\Models\PmGoalSetting;
 use App\Models\Project;
@@ -33,43 +34,6 @@ class MilestoneAddedEventListener
      */
     public function handle(MilestoneAddedEvent $event)
     {
-        // check if total budget has been completed
-        // if not then pm goal will not be created
-
-        // get total milestone amount
-        $milestone = $event->milestone;
-        $milestoneSum = ProjectMilestone::where('project_id', $milestone->project_id)->sum('cost');
-        $project = Project::find($milestone->project_id);
-
-        // dd($milestoneSum, $project->project_budget, $milestoneSum < $project->project_budget);
-        if ($milestoneSum < $project->project_budget) {
-            return;
-        }
-
-        $lastGoal = ProjectPmGoal::where('project_id', $milestone->project_id)->orderBy('goal_end_date', 'desc')->first();
-
-        $goalCodes = ProjectPmGoal::$goalCodes['upsell'];
-
-        // dd($milestone);
-
-        if($milestone->cost < 100) $goalDuration = 3;
-        else $goalDuration = 3 +  (int) (($milestone->cost -100) / 100);
-
-        $goal = new ProjectPmGoal();
-        $goal->project_id = $lastGoal->project_id;
-        $goal->client_id = $lastGoal->client_id;
-        $goal->pm_id = $lastGoal->pm_id;
-        $goal->project_type = $lastGoal->project_type;
-        $goal->project_category = $lastGoal->project_category;
-
-        $goal->goal_code = $goalCodes['code'];
-        $goal->goal_name = $goalCodes['name'];
-        $goal->goal_type = $goalCodes['type'];
-
-        $goal->goal_start_date = $lastGoal->goal_start_date;
-        $goal->goal_end_date = Carbon::parse($lastGoal->goal_start_date)->addDay($lastGoal->duration + $goalDuration);
-        $goal->duration = $lastGoal->duration + $goalDuration;
-        $goal->added_by = Auth::user()->id;
-        $goal->save();
+        (new HelperPmProjectStatusController)->upsellGoalCreation($event->milestone);
     }
 }
