9e8d307ddb3be209ac8410ec7e7fba719e6951c4
Saidul Islam <saidul@seopage1.net>
2024-05-23 12:04:17 +0600

    Regular point logic implement to PM incentive

--- app/Console/Commands/DisbursePmIncentiveMonthly.php
+++ app/Console/Commands/DisbursePmIncentiveMonthly.php
@@ -59,8 +59,8 @@ class DisbursePmIncentiveMonthly extends Command
             ->whereBetween('task_revisions.created_at', [$startDate, $endDate])
             ->count();
 
-            $revision_percent = number_format(( $pm_revisions / $total_tasks ) * 100, 2);
-            // Incentive::progressiveStore(1, $user->id, $revision_percent, $now);
+            $revision_percent = $total_tasks ? number_format(( $pm_revisions / $total_tasks ) * 100, 2) : 0;
+            Incentive::progressiveStore(1, $user->id, $revision_percent, $now);
             // End
 
             // Goal achieve rate
@@ -81,12 +81,16 @@ class DisbursePmIncentiveMonthly extends Command
             // Negative vs poisitive point
             $total_points = $totalEarnedPoints + $totalLostPoints;
             $negative_point_rete = $total_points ? number_format(($totalLostPoints / $total_points) * 100, 2) : 0;
+            Incentive::progressiveStore(3, $user->id, $negative_point_rete, $now);
+            // End
 
             // Percentage of delayed project
             $delayed_project_percentage = Project::selectRaw('FORMAT((SUM(CASE WHEN p_m_projects.delayed_status = 1 THEN 1 ELSE 0 END) / SUM(CASE WHEN p_m_projects.delayed_status = NULL THEN 0 ELSE 1 END)) * 100, 2) as delayed_project_percentage')
             ->join('p_m_projects', 'p_m_projects.project_id', '=', 'projects.id')
             ->where([['projects.pm_id', $user->id],['projects.status', 'in progress'],['projects.project_status', 'Accepted']])
             ->first()->delayed_project_percentage;
+            Incentive::progressiveStore(4, $user->id, $delayed_project_percentage, $now);
+            // End
             
             // Milestone cancelation rate
             $milestone_cancelation_rate = Project::selectRaw('FORMAT((SUM(CASE WHEN project_milestones.status = "canceled" THEN 1 ELSE 0 END) / SUM(CASE WHEN project_milestones.status = "complete" THEN 1 ELSE 0 END)) * 100, 2) as milestone_cancelation_rate')
@@ -94,17 +98,24 @@ class DisbursePmIncentiveMonthly extends Command
             ->where([['projects.pm_id', $user->id],['projects.status', 'in progress'],['projects.project_status', 'Accepted']])
             ->whereBetween('project_milestones.created_at', [Carbon::now()->startOfMonth(), Carbon::now()->endOfMonth()])
             ->first()->milestone_cancelation_rate;
+            Incentive::progressiveStore(5, $user->id, $milestone_cancelation_rate, $now);
+            // End
 
             // Project dateline miss rate
             $projects = Project::where([['projects.pm_id', $user->id],['projects.status', 'in progress'],['projects.project_status', 'Accepted']])->get();
-            $deadline_miss_rate = number_format(($projects->where('deadline', '<', now())->count() / $projects->count()) * 100, 2);
-            
+            $deadline_miss_rate = $projects->count() ? number_format(($projects->where('deadline', '<', now())->count() / $projects->count()) * 100, 2) : 0;
+            Incentive::progressiveStore(6, $user->id, $deadline_miss_rate, $now);
+            // End
+
             // Client retention rate 
             $pm_created_clients = Project::where('added_by', $user->id)->where('created_at', '>=', Carbon::now()->startOfMonth())->pluck('client_id');
             $pm_assigned_clients = Project::where('pm_id', $user->id)->whereIn('client_id', $pm_created_clients)->where('created_at', '>=', Carbon::now()->startOfMonth())->pluck('client_id')->toArray();
             $retension_this_month = count(array_keys(array_filter(array_count_values($pm_assigned_clients), fn($count) => $count > 1)));
-            $client_retention_rate = number_format(((Project::whereIn('client_id', $pm_created_clients)->where('pm_id', $user->id)->where('created_at', '<=', Carbon::now()->startOfMonth())->pluck('client_id')->count() + $retension_this_month) / $pm_created_clients->count())*100, 2);
-            dd($client_retention_rate);
+            $client_retention_rate = $pm_created_clients->count() ? number_format(((Project::whereIn('client_id', $pm_created_clients)->where('pm_id', $user->id)->where('created_at', '<=', Carbon::now()->startOfMonth())->pluck('client_id')->count() + $retension_this_month) / $pm_created_clients->count())*100, 2) : 0;
+            Incentive::progressiveStore(7, $user->id, $client_retention_rate, $now);
+            // End
+
+            dd('Success');
         }
     }
 }
