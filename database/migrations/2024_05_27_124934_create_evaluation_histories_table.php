099812edc7cc7352aa19b87cfbbe030101177516
mdrayhanseopage1 <rayhan@seopage1.net>
2024-05-27 13:33:32 +0600

    history_data_send_to_api

---
+++ database/migrations/2024_05_27_124934_create_evaluation_histories_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('evaluation_histories', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->dateTime('start_date')->nullable();
+            $table->dateTime('exp_date')->nullable();
+            $table->decimal('communication')->nullable();
+            $table->decimal('professionalism')->nullable();
+            $table->decimal('identiey_issues')->nullable();
+            $table->decimal('dedication')->nullable();
+            $table->decimal('obedience')->nullable();
+            $table->decimal('lead_dev_avg_rating')->nullable();
+            $table->longText('team_lead_cmnt')->nullable();
+            $table->longText('managements_cmnt')->nullable();
+            $table->string('managements_decision')->nullable();
+            $table->integer('managements_id')->nullable();
+            $table->string('managements_name')->nullable();
+            $table->dateTime('managements_auth_at')->nullable();
+            $table->dateTime('accept_rejected')->nullable();
+            $table->integer('ld_submission_status')->default(0);
+            $table->integer('employee_status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('evaluation_histories');
+    }
+};
