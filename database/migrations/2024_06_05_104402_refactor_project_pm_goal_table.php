bfd2814f1c5b62e9e57e04e846ce9c38eec54d86
mdrayhanseopage1 <rayhan@seopage1.net>
2024-06-05 11:54:15 +0600

    table_modify

--- database/migrations/2024_06_05_104402_refactor_project_pm_goal_table.php
+++ database/migrations/2024_06_05_104402_refactor_project_pm_goal_table.php
@@ -15,14 +15,21 @@ return new class extends Migration
     public function up()
     {
         ProjectPmGoal::truncate();
+        
+        // drop columns
+        $drop_columns =['rating','client_communication_rating','is_client_communication','admin_comment', 'suggestion','is_any_negligence','negligence_pm_rating','admin_id','updated_by','resolved_status','client_communication','description','negligence_pm','reason','extended_day','extended_goal_end_day','extended_request_status'];
+        foreach ($drop_columns as $item) {
+            if (Schema::hasColumn('project_pm_goals', $item)) Schema::dropColumns('project_pm_goals', $item);
+        }
 
-        Schema::dropColumns('project_pm_goals', ['rating','client_communication_rating','negligence_pm_rating','admin_id','updated_by','resolved_status','client_communication','description','negligence_pm','reason','extended_day','extended_goal_end_day','extended_request_status']);
-
+        // add columns
         Schema::table('project_pm_goals', function (Blueprint $table) {
             $table->longText('expired_meet_description')->nullable()->after('goal_status');
             $table->integer('expired_status')->default(0)->after('expired_meet_description');
+            $table->integer('mail_status')->default(0)->after('expired_status');
         });
 
+        // change columns
         Schema::table('project_pm_goals', function(Blueprint $table){
             $table->integer('extension_status')->default(0)->comment('1 = pending, 2 = accepted, 3 = rejected')->change();
             $table->integer('reason_status')->default(0)->comment('1 = pending, 2 = accepted')->change();
