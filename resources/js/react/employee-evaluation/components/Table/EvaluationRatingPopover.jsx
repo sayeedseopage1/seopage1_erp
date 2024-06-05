3454974321ab5f66d88b5fc537d35674441b8516
tanvirmitul1 <mitulseopage1@gmail.com>
2024-06-05 15:25:55 +0600

    rating button message fix

--- resources/js/react/employee-evaluation/components/Table/EvaluationRatingPopover.jsx
+++ resources/js/react/employee-evaluation/components/Table/EvaluationRatingPopover.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import { convertTime } from "../../../utils/converTime";
 
 const EvaluationRatingPopover = ({ data }) => {
+    // Determine the color based on data?.total_min
+    const submissionStatusColor =
+        data && Number(data.total_min) > 60 ? "green" : "red";
+
     return (
         <div className="revision_popover_panel">
-            <div style={{ color: "red" }}>
-                <div>{`Tracked Hour: ${convertTime(data?.total_min)}`}</div>
+            <div>
+                <div
+                    style={{ color: submissionStatusColor }}
+                >{`Tracked Hour: ${convertTime(data?.total_min)}`}</div>
 
-                <div>Submission Status: Not Submitted</div>
+                <div style={{ color: "red" }}>
+                    Submission Status:{" "}
+                    <span>{data?.task_board_column_name}</span>
+                </div>
             </div>
             <br />
             <div>Hence, this task is not applicable for rating</div>
