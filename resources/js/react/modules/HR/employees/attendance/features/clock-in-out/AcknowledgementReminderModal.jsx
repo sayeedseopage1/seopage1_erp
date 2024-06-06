92e17889c5c79dbd35843476fecd0be5ef0fcab1
tanvirmitul1 <mitulseopage1@gmail.com>
2024-06-06 12:49:53 +0600

    random commit

--- resources/js/react/modules/HR/employees/attendance/features/clock-in-out/AcknowledgementReminderModal.jsx
+++ resources/js/react/modules/HR/employees/attendance/features/clock-in-out/AcknowledgementReminderModal.jsx
@@ -20,40 +20,57 @@ const dayjs = new CompareDate();
 const DateFormat = (date) => {
     const d = {
         unFormatted: date,
-        formatted: dayjs.dayjs(date).isSame(dayjs.dayjs(), 'day') ? 'Today' : dayjs.dayjs(date).format('MMM DD, YYYY')
-    }
+        formatted: dayjs.dayjs(date).isSame(dayjs.dayjs(), "day")
+            ? "Today"
+            : dayjs.dayjs(date).format("MMM DD, YYYY"),
+    };
 
     return d;
 };
 
-const AcknowledgementReminderModal = ({ close, title = "Stop timer", reminderDate, reminderType, data, onSubmit }) => {
+const AcknowledgementReminderModal = ({
+    close,
+    title = "Stop timer",
+    reminderDate,
+    reminderType,
+    data,
+    projectTimeLog,
+    onSubmit,
+}) => {
     const [step, setStep] = React.useState(0);
     const [isSubmitting, setIsSubmitting] = React.useState(false);
 
-
+    console.log("acknowledge data", data);
     // handle form submission
     const handleSubmitForm = async (data, submissionType, cb) => {
         setIsSubmitting(true);
 
-        try{
-            await axios.post('/account/developer/daily-minimum-track-hours-log/acknowledgement', {
-                ...data,
-                date: dayjs.dayjs(reminderDate).format('YYYY-MM-DD'),
-                _token: document.querySelector("meta[name='csrf-token']").getAttribute('content')
-            }).then(res => {
-                if(submissionType !== 'CONTINUE'){
-                    onSubmit();
-                    close();
-                }
+        try {
+            await axios
+                .post(
+                    "/account/developer/daily-minimum-track-hours-log/acknowledgement",
+                    {
+                        ...data,
+                        date: dayjs.dayjs(reminderDate).format("YYYY-MM-DD"),
+                        _token: document
+                            .querySelector("meta[name='csrf-token']")
+                            .getAttribute("content"),
+                    }
+                )
+                .then((res) => {
+                    if (submissionType !== "CONTINUE") {
+                        onSubmit();
+                        close();
+                    }
 
-                cb();
-            })
+                    cb();
+                });
 
             setIsSubmitting(false);
-        }catch(error){
-            console.log(error)
+        } catch (error) {
+            console.log(error);
         }
-    }
+    };
 
     return (
         <div className="sp1_single_task--modal">
@@ -64,7 +81,12 @@ const AcknowledgementReminderModal = ({ close, title = "Stop timer", reminderDat
                 >
                     {/* modal header */}
                     <div className="border-bottom pb-2 px-3 d-flex align-items-center justify-content-between">
-                        <div className="font-weight-bold f-16">{reminderType} on <span style={{color: 'red'}}>{DateFormat(reminderDate).formatted}</span></div>
+                        <div className="font-weight-bold f-16">
+                            {reminderType} on{" "}
+                            <span style={{ color: "red" }}>
+                                {DateFormat(reminderDate).formatted}
+                            </span>
+                        </div>
                         <Button variant="tertiary" onClick={close}>
                             <i className="fa-solid fa-xmark" />
                         </Button>
@@ -73,9 +95,26 @@ const AcknowledgementReminderModal = ({ close, title = "Stop timer", reminderDat
                     {/* modal body */}
                     <div className="sp1_single_task--modal-body sp1_single_task-modal-body-options p-3">
                         <div className="alert alert-warning">
-                            Your tracked time for <strong>{DateFormat(reminderDate).formatted}</strong> is <strong>{convertTime(data?.data?.complete_hours)}</strong>. Your minimum tracked hours should have been <strong>{convertTime(data?.data?.target_minimum_log_hours)}</strong>, and it is <strong>{convertTime(data?.data?.incomplete_hours)}</strong> less.
+                            Your tracked time for{" "}
+                            <strong>
+                                {DateFormat(reminderDate).formatted}
+                            </strong>{" "}
+                            is{" "}
+                            <strong>
+                                {convertTime(data?.data?.complete_hours)}
+                            </strong>
+                            . Your minimum tracked hours should have been{" "}
+                            <strong>
+                                {convertTime(
+                                    data?.data?.target_minimum_log_hours
+                                )}
+                            </strong>
+                            , and it is{" "}
+                            <strong>
+                                {convertTime(data?.data?.incomplete_hours)}
+                            </strong>{" "}
+                            less.
                         </div>
-
 
                         {/* options */}
                         <div className="sp1_stop-button-confirmation-option">
@@ -83,11 +122,15 @@ const AcknowledgementReminderModal = ({ close, title = "Stop timer", reminderDat
                             <div className="confirmation--options">
                                 <Switch>
                                     {/* option: I did not have enough work to do. */}
-                                    <Switch.Case condition={!step || step === 1}>
+                                    <Switch.Case
+                                        condition={!step || step === 1}
+                                    >
                                         <Option1
                                             checked={step === 1}
                                             index={1}
-                                            onChange={e => setStep(Number(e.target.value))}
+                                            onChange={(e) =>
+                                                setStep(Number(e.target.value))
+                                            }
                                             onSubmit={handleSubmitForm}
                                             onBack={() => setStep(0)}
                                             isLoading={isSubmitting}
@@ -95,11 +138,15 @@ const AcknowledgementReminderModal = ({ close, title = "Stop timer", reminderDat
                                     </Switch.Case>
 
                                     {/* option: During transition from one task to another, I had to wait for a while. */}
-                                    <Switch.Case condition={!step || step === 2}>
+                                    <Switch.Case
+                                        condition={!step || step === 2}
+                                    >
                                         <Option2
                                             checked={step === 2}
                                             index={2}
-                                            onChange={e => setStep(Number(e.target.value))}
+                                            onChange={(e) =>
+                                                setStep(Number(e.target.value))
+                                            }
                                             onSubmit={handleSubmitForm}
                                             onBack={() => setStep(0)}
                                             isLoading={isSubmitting}
@@ -107,24 +154,35 @@ const AcknowledgementReminderModal = ({ close, title = "Stop timer", reminderDat
                                     </Switch.Case>
 
                                     {/* option3: I was present less hours at work today or last day  */}
-                                    <Switch.Case condition={!step || step === 3}>
+                                    <Switch.Case
+                                        condition={!step || step === 3}
+                                    >
                                         <Option3
                                             checked={step === 3}
                                             index={3}
-                                            onChange={e => setStep(Number(e.target.value))}
+                                            onChange={(e) =>
+                                                setStep(Number(e.target.value))
+                                            }
                                             onSubmit={handleSubmitForm}
                                             onBack={() => setStep(0)}
-                                            checkInTime={DateFormat(reminderDate).formatted}
+                                            checkInTime={
+                                                DateFormat(reminderDate)
+                                                    .formatted
+                                            }
                                             isLoading={isSubmitting}
                                         />
                                     </Switch.Case>
 
                                     {/* option4: I was present less hours at work today or last day  */}
-                                    <Switch.Case condition={!step || step === 4}>
+                                    <Switch.Case
+                                        condition={!step || step === 4}
+                                    >
                                         <Option4
                                             checked={step === 4}
                                             index={4}
-                                            onChange={e => setStep(Number(e.target.value))}
+                                            onChange={(e) =>
+                                                setStep(Number(e.target.value))
+                                            }
                                             onSubmit={handleSubmitForm}
                                             onBack={() => setStep(0)}
                                             isLoading={isSubmitting}
@@ -132,11 +190,15 @@ const AcknowledgementReminderModal = ({ close, title = "Stop timer", reminderDat
                                     </Switch.Case>
 
                                     {/* option5: I forgot to track hours. */}
-                                    <Switch.Case condition={!step || step === 5}>
+                                    <Switch.Case
+                                        condition={!step || step === 5}
+                                    >
                                         <Option5
                                             checked={step === 5}
                                             index={5}
-                                            onChange={e => setStep(Number(e.target.value))}
+                                            onChange={(e) =>
+                                                setStep(Number(e.target.value))
+                                            }
                                             onSubmit={handleSubmitForm}
                                             onBack={() => setStep(0)}
                                             isLoading={isSubmitting}
@@ -144,17 +206,20 @@ const AcknowledgementReminderModal = ({ close, title = "Stop timer", reminderDat
                                     </Switch.Case>
 
                                     {/* option6: In the morning, I had to wait for work before I could start.. */}
-                                    <Switch.Case condition={!step || step === 6}>
+                                    <Switch.Case
+                                        condition={!step || step === 6}
+                                    >
                                         <Option6
                                             checked={step === 6}
                                             index={6}
-                                            onChange={e => setStep(Number(e.target.value))}
+                                            onChange={(e) =>
+                                                setStep(Number(e.target.value))
+                                            }
                                             onSubmit={handleSubmitForm}
                                             onBack={() => setStep(0)}
                                             isLoading={isSubmitting}
                                         />
                                     </Switch.Case>
-
                                 </Switch>
                             </div>
                         </div>
