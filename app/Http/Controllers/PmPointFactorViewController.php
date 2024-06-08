ee838f9c80b099da93794283ff507b6df3c745db
Saidul Islam <saidul@seopage1.net>
2024-06-08 12:49:08 +0600

    Set permission for PM point criteria and history

--- app/Http/Controllers/PmPointFactorViewController.php
+++ app/Http/Controllers/PmPointFactorViewController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\AccountBaseController;
 
 class PmPointFactorViewController extends AccountBaseController
@@ -16,6 +17,7 @@ class PmPointFactorViewController extends AccountBaseController
 
     public function __invoke(Request $request)
     {
+        abort_403(!(Auth::user()->role_id == 1 || Auth::user()->role_id == 4));
         return view('pm-point-factor.index', $this->data);
     }
 }
