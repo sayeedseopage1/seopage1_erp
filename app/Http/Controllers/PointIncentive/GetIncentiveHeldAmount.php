dec982f369af1dc557cc62e7911c982a418a3904
Saidul Islam <saidul@seopage1.net>
2024-06-10 17:34:43 +0600

    Sending held amount data as response for api

--- app/Http/Controllers/PointIncentive/GetIncentiveHeldAmount.php
+++ app/Http/Controllers/PointIncentive/GetIncentiveHeldAmount.php
@@ -21,7 +21,7 @@ class GetIncentiveHeldAmount extends Controller
         $userId = $request->user_id??null;
         $startDate = $request->start_date ? Carbon::parse($request->start_date)->startOfDay() : Carbon::now()->startOfYear();
         $endDate = Carbon::parse($request->end_date??now())->endOfYear();
-        return $data = IncentivePayment::where('user_id', $userId)->whereBetween('date', [$startDate, $endDate])->get()->map(function ($item) {
+        $data = IncentivePayment::where('user_id', $userId)->whereBetween('date', [$startDate, $endDate])->get()->map(function ($item) {
             $monthYear = Carbon::parse($item->date)->format('F, Y');
             $item->title = "Monthly Incentive (for the month of $monthYear)";
             $item->viewing_month = Carbon::parse($item->date)->addDays(5)->format('F, Y');
@@ -29,5 +29,10 @@ class GetIncentiveHeldAmount extends Controller
             $item->held_amount_payment = $heldAmountPaymentHistory ? "Held incentive amount ". $heldAmountPaymentHistory->paid_amount . " taka for ". Carbon::parse($heldAmountPaymentHistory->from_month)->format('F, Y') . " - ". Carbon::parse($heldAmountPaymentHistory->to_month)->format('F, Y') . " has been paid on ". Carbon::parse($heldAmountPaymentHistory->payment_date)->format('F j, Y') . " by " . $heldAmountPaymentHistory->addedBy->name : null;
             return $item;
         });
+
+        return response()->json([
+            'status'=> 200,
+            'data' => $data
+        ]);
     }
 }
