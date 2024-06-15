e358a3d31da44d8154836a189895cf30a8c2a95c
moonseopage1 <moon@seopage1.net>
2024-06-15 13:43:42 +0600

    [WIP] chat list item avatar

--- resources/js/react/modules/marketplace/components/freelancerMessage/ui/ChatListItem.jsx
+++ resources/js/react/modules/marketplace/components/freelancerMessage/ui/ChatListItem.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import userProfile from '../../../assets/freelancerMessage/user_avatar_2.png'
+import chatActiveIcon from '../../../assets/freelancerMessage/chat-active.svg'
 
 const ChatListItem = () => {
     return (
         <div>
-            <div>
-                <img src={userProfile} alt="userProfile" />
+            {/* TODO: active status position fix */}
+            <div className='chat_user_avatar_wrapper'>
+                <img src={userProfile} alt="userProfile" className='chat_list_user_avatar' />
+                <img src={chatActiveIcon} alt="chatActiveIcon" className='active_status' />
             </div>
             <div>
                 <div></div>
