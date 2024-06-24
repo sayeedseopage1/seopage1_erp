4b55aa5543294273b1bb4f551ca64404ac4865ee
Saidul Islam <saidul@seopage1.net>
2024-06-24 09:45:57 +0600

    projectTaskList modified for project dashboard by hours logged

--- app/Http/Controllers/Project/ProjectInsightController.php
+++ app/Http/Controllers/Project/ProjectInsightController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers\Project;
 
-use App\Http\Controllers\Controller;
-use App\Models\ProjectMilestone;
+use Carbon\Carbon;
 use App\Models\Task;
+use App\Models\Subtask;
 use Illuminate\Http\Request;
+use App\Models\ProjectMilestone;
+use App\Http\Controllers\Controller;
 
 class ProjectInsightController extends Controller
 {
@@ -21,7 +23,49 @@ class ProjectInsightController extends Controller
 
     public function getProjectTasks($project_id)
     {
-        $data = Task::select(['id','project_id','heading','start_date','due_date','original_due_date','status','board_column_id','estimate_hours','estimate_minutes'])->where('project_id', $project_id)->whereNull('subtask_id')->get();
+        $data = Task::select(['id','project_id','heading','start_date','due_date','original_due_date','status','board_column_id','estimate_hours','estimate_minutes'])->where('project_id', $project_id)->whereNull('subtask_id')->get()->map(function($row){
+            $timeLog = '--';
+            if($row->timeLogged) {
+                $totalMinutes = $row->timeLogged->sum('total_minutes');
+                foreach($row->timeLogged as $value) {
+                    if (is_null($value->end_time)) {
+                        $workingTime = $value->start_time->diffInMinutes(Carbon::now());
+                        $totalMinutes = $totalMinutes + $workingTime;
+                    }
+                }
+                $breakMinutes = $row->breakMinutes();
+                $totalMinutes = $totalMinutes - $breakMinutes;
+                $timeLog = intdiv($totalMinutes, 60) . ' ' . __('app.hrs') . ' ';
+                if ($totalMinutes % 60 > 0) {
+                    $timeLog .= $totalMinutes % 60 . ' ' . __('app.mins');
+                }
+            }
+            
+            $tas_id = Task::where('id',$row->id)->first();
+            $subtasks = Subtask::where('task_id', $tas_id->id)->get();
+            
+            foreach ($subtasks as $subtask) {
+                $task = Task::where('subtask_id', $subtask->id)->first();
+                $totalMinutes = $totalMinutes + $task->timeLogged->sum('total_minutes');
+                foreach($task->timeLogged as $value) {
+                    if (is_null($value->end_time)) {
+                        $workingTime = $value->start_time->diffInMinutes(Carbon::now());
+                        $totalMinutes = $totalMinutes + $workingTime;
+                    }
+                }
+            }
+            
+            if($subtasks == null) {
+                $row->hours_logged = $timeLog;
+            } else {
+                $timeL = intdiv(($totalMinutes), 60) . ' ' . __('app.hrs') . ' ';
+                if ($totalMinutes % 60 > 0) {
+                    $timeL .= ($totalMinutes) % 60 . ' ' . __('app.mins');
+                }
+                $row->hours_logged =  $timeL;
+            }
+            return $row;
+        });
 
         return response()->json([
             'code' => 200,
