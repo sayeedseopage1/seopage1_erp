b76a9a706bbbf54a441d1574ce0b87d30ffebb61
moonseopage1 <moon@seopage1.net>
2024-06-26 11:37:13 +0600

    [WIP] project details clarification board section

--- resources/js/react/modules/marketplace/components/freelancerProjectDetails/sections/ProjectDetails.jsx
+++ resources/js/react/modules/marketplace/components/freelancerProjectDetails/sections/ProjectDetails.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import '../../../styles/freelancerProjectDetails/projectDetails.css'
 import { dummy_projects } from '../../../constants/projects';
 import flagIcon from '../../../assets/freelancerProjectDetails/flag.svg';
+import { Input } from 'antd';
+
+const { TextArea } = Input;
 
 const ProjectDetails = () => {
   //!! TODO: This is used development only for now, It should be removed in production
   const singleProject = dummy_projects[0];
 
-  const { id, project_id, title, description, details, currency, budget_range, highest_bid_amount, bids_count, average_rating, reviews_count, skills, created_at, updated_at } = singleProject || {};
+  const { id, project_id, title, description, details, currency, budget_range, highest_bid_amount, bids_count, average_rating, reviews_count, skills, client_info, created_at, updated_at } = singleProject || {};
+
+  const { profile_image_url, name, location, member_since, client_engagement, client_verification } = client_info || {};
 
   return (
     <div className='p_d_wrapper'>
@@ -41,13 +46,35 @@ const ProjectDetails = () => {
                 <p>Report Project</p>
               </button>
             </div>
+            <div className='p_d_scam_alert_wrapper'>
+              <p className='sp1_marketplace_default_text mb-1'>Beware of scams</p>
+              <p className='sp1_marketplace_default_text'>If you are being asked to pay a security deposit, or if you are being asked to chat on Telegram, WhatsApp, or another messaging platform,
+                it is likely a scam. Report these projects or contact Support for assistance.
+              </p>
+              <button className='p_d_scam_alert_btn'>Learn more</button>
+            </div>
           </div>
         </div>
         <div className='p_d_content_wrapper'>
-          bottom section
+          <div className='p_d_title_wrapper'>
+            <h4 className='p_d_title_600'>Clarification Board:</h4>
+            <h4 className='sp1_marketplace_default_text'>No spam, self-promotion or advertisement is permitted.</h4>
+          </div>
+          <div className='p_d_clarification_board'>
+            <img className='p_d_client_image' src={profile_image_url} alt={name} />
+            <div className='p_d_clarification_board_input'>
+              <TextArea
+                placeholder="Ask a question....."
+                autoSize={{ minRows: 2, maxRows: 6 }}
+                className='message_input'
+              />
+            </div>
+          </div>
         </div>
       </div>
-      <div className='p_d_content_wrapper p_d_content_right'> side bar</div>
+      <div className='p_d_content_wrapper p_d_content_right'>
+
+      </div>
     </div>
   );
 };
