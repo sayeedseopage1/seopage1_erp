e6652f1f25fff4c224f282fbad2c775e65d93e75
Saidul Islam <saidul@seopage1.net>
2024-06-26 16:47:08 +0600

    param added with pending action for project dashboard

--- app/Models/PendingAction.php
+++ app/Models/PendingAction.php
@@ -19,11 +19,17 @@ class PendingAction extends Model
                 $buttonName = 'explain_dispute';
             }elseif($pendingAction->code == 'DFA'){
                 $buttonName = 'project_dispute_authorization';
+            }elseif($pendingAction->code == 'TGA'){
+                $buttonName = 'pm_task_guidline_authorization';
+            }elseif($pendingAction->code == 'QCA'){
+                $buttonName = 'project_qc_authorization';
+            }elseif($pendingAction->code == 'PCA'){
+                $buttonName = 'completion_form_authorization';
             }
             
             if($buttonName){
                 $button = json_decode($pendingAction->button);
-                $button[0]->button_url = $button[0]->button_url.'?modal='.$buttonName;
+                $button[0]->button_url = $button[0]->button_url.'?modal_type='.$buttonName;
                 $pendingAction->button = json_encode($button);
                 $pendingAction->save();
             }
