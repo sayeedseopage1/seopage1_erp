0d836f516bc02c9edbf4d69b16d5e16d3d128c16
moonseopage1 <moon@seopage1.net>
2024-06-27 14:09:13 +0600

    [ADD] tasklist page done

--- resources/js/react/modules/marketplace/components/freelancerProjectDetails/sections/Tasklists.jsx
+++ resources/js/react/modules/marketplace/components/freelancerProjectDetails/sections/Tasklists.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import taskListImage from '../../../assets/freelancerProjectDetails/task_list.png'
 
 const Tasklists = () => {
     return (
-        <div>
-            <h1>Tasklists</h1>
+        <div className='p_d_content_wrapper task_list_page_wrapper'>
+            <div>
+                <img src={taskListImage} alt="taskListImage" />
+            </div>
+            <div className='task_lis_page_content'>
+                <h2 className='task_list_page_title'>Get more done, faster!</h2>
+                <p className='task_list_page_description'>Organize your work, set deadlines, prioritize and assign tasks.</p>
+                <button className='task_list_page_btn'>Add Tasklist</button>
+            </div>
         </div>
     );
 };
