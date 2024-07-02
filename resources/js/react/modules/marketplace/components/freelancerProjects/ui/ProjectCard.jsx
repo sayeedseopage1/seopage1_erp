7e36fed06338f7c6551a81d10a88e10cc092f67e
moonseopage1 <moon@seopage1.net>
2024-07-02 12:56:37 +0600

    [FIX] some modification in project page(expanded)

--- resources/js/react/modules/marketplace/components/freelancerProjects/ui/ProjectCard.jsx
+++ resources/js/react/modules/marketplace/components/freelancerProjects/ui/ProjectCard.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import archiveIcon from '../../../assets/freelancerProjects/archive-tick.svg';
 import commentIcon from '../../../assets/freelancerProjects/comment-icon.svg';
 import moment from 'moment';
 import FractionalRating from '../../commonComponents/FractionalRating';
 
 const ProjectCard = ({ item }) => {
-    const { title, description, currency, budget_range, average_bid_amount, bids_count, average_rating, reviews_count, skills, created_at } = item || {};
+    const { title, description, details, currency, budget_range, average_bid_amount, bids_count, average_rating, reviews_count, skills, created_at } = item || {};
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const toggleDescription = () => {
+        setIsExpanded(!isExpanded);
+    };
     return (
         <div className='project_card_wrapper'>
             <div className='project_card_header'>
@@ -17,7 +22,17 @@ const ProjectCard = ({ item }) => {
             </div>
             <div className='project_card_body'>
                 <p className='project_normal_text'>Budget {budget_range} {currency?.code}</p>
-                <p className='project_normal_text project_card_description'>{description}</p>
+                <div className='project_normal_text project_card_description'>
+                    <span dangerouslySetInnerHTML={{ __html: details }} className={isExpanded ? 'expanded' : 'truncated'}>
+                        {/* {description} */}
+                    </span>
+                    {!isExpanded && (
+                        <span className='more-btn' onClick={toggleDescription}> more</span>
+                    )}
+                    {/* {isExpanded && (
+                        <span className='less-btn' onClick={toggleDescription}> less</span>
+                    )} */}
+                </div>
                 <div className='project_card_skills'>
                     {
                         skills.map((skill, index) => (
