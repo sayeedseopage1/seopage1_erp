4f51614eb31d5d6a89a0e7f4b73259d35f8c1f9d
mdrayhanseopage1 <rayhan@seopage1.net>
2024-07-03 15:16:03 +0600

    time extend

--- app/Http/Controllers/IndependentTaskController.php
+++ app/Http/Controllers/IndependentTaskController.php
@@ -237,7 +237,7 @@ class IndependentTaskController extends AccountBaseController
                         $evaluation->start_date = Carbon::now();
                         $emp_start_task = $evaluation->start_date;
 
-                        $exp_date = Carbon::parse($emp_start_task)->addMinutes(30);
+                        $exp_date = Carbon::parse($emp_start_task)->addMinutes(70);
                         $countSundays = 0;
                         $currentDate = $emp_start_task->copy(); 
                         while ($currentDate->lte($exp_date)) {
@@ -247,7 +247,7 @@ class IndependentTaskController extends AccountBaseController
                             $currentDate->addDay(); 
                         }
                         
-                        $evaluation->exp_date = Carbon::parse($emp_start_task)->addMinutes(30 + $countSundays);
+                        $evaluation->exp_date = Carbon::parse($emp_start_task)->addMinutes(70 + $countSundays);
                         
                         $evaluation->save();
                     }
