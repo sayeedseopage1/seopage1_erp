d3c9fd875f693dd2369db94036039921604e2eb7
tanvirmitul1 <mitulseopage1@gmail.com>
2024-07-06 08:16:18 +0600

    drag drop add

--- resources/js/react/portfolios/index.jsx
+++ resources/js/react/portfolios/index.jsx
@@ -1,24 +1,75 @@
-import React, { Suspense } from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
-import { store } from '../services/store';
-import Portfolio from './Portfolio';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import { store } from "../services/store";
+import Portfolio from "./Portfolio";
 
+import { DndProvider, useDragLayer } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
+// custom drag layer
+const DragLayer = () => {
+    const { item, itemType, currentOffset } = useDragLayer((monitor) => ({
+        item: monitor.getItem(),
+        itemType: monitor.getItemType(),
+        currentOffset: monitor.getClientOffset(),
+    }));
+
+    if (!currentOffset) {
+        return null;
+    }
+    return (
+        <div
+            style={{
+                position: "fixed",
+                pointerEvents: "none",
+                zIndex: 999999,
+                left: currentOffset.x,
+                top: currentOffset.y,
+            }}
+        >
+            {/* Render your custom preview here based on the dragged item */}
+            {itemType === "column" && (
+                <div
+                    className="py-2 px-2 pl-3 bg-white shadow border"
+                    style={{ width: item.columnDef.size }}
+                >
+                    {item.columnDef.header}
+                </div>
+            )}
+        </div>
+    );
+};
+
+const Container = () => {
+    return (
+        <React.Fragment>
+            {/* <Toaster /> */}
+            <DragLayer />
+            <Outlet />
+        </React.Fragment>
+    );
+};
 
 const container = document.getElementById("portfolioPageContainer");
 
-// custom drag layer 
-if(container){
-  ReactDOM.createRoot(container).render(
-    <React.StrictMode>
-     <Provider store={store}>
-        <BrowserRouter>
-            <Routes>
-                <Route path='/account/portfolio' element={<Portfolio />} />
-            </Routes>
-        </BrowserRouter> 
-     </Provider>
-    </React.StrictMode>
-  )
+// custom drag layer
+if (container) {
+    ReactDOM.createRoot(container).render(
+        <React.StrictMode>
+            <Provider store={store}>
+                <DndProvider backend={HTML5Backend}>
+                    <BrowserRouter>
+                        <Routes>
+                            <Route
+                                path="/account/portfolio"
+                                element={<Portfolio />}
+                            />
+                        </Routes>
+                    </BrowserRouter>
+                </DndProvider>
+            </Provider>
+        </React.StrictMode>
+    );
 }
