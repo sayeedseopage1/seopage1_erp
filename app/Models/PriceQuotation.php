0dc176825071e8a70a9b23eb7dae2397eef0211c
Saidul Islam <saidul@seopage1.net>
2024-07-08 12:14:27 +0600

    Price quotation resource migration and model added

--- app/Models/PriceQuotation.php
+++ app/Models/PriceQuotation.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class PriceQuotation extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'deal_stage_id',
+        'project_cms_id',
+        'project_niche_id',
+        'no_of_primary_pages',
+        'no_of_secondary_pages',
+        'no_of_major_functionalities',
+        'risk_factor',
+        'total_hours_of_others_works',
+        'currency_id',
+        'deadline_type',
+        'deadline',
+        'platform_account_id',
+        'calculated_budget',
+        'project_budget'
+    ];
 }
--- database/migrations/2024_07_06_125651_create_price_quotations_table.php
+++ database/migrations/2024_07_06_125651_create_price_quotations_table.php
@@ -20,6 +20,15 @@ return new class extends Migration
             $table->foreignId('project_niche_id')->nullable()->constrained();
             $table->unsignedTinyInteger('no_of_primary_pages')->default(0);
             $table->unsignedTinyInteger('no_of_secondary_pages')->default(0);
+            $table->unsignedTinyInteger('no_of_major_functionalities')->nullable();
+            $table->decimal('risk_factor')->nullable();
+            $table->decimal('total_hours_of_others_works')->nullable();
+            $table->foreignId('currency_id')->nullable()->constrained();
+            $table->unsignedTinyInteger('deadline_type')->default(1)->comment('1 = Flexible, 2 = Fixed');
+            $table->timestamp('deadline')->nullable();
+            $table->foreignId('platform_account_id')->nullable()->constrained();
+            $table->decimal('calculated_budget')->default(0);
+            $table->decimal('project_budget');
             $table->timestamps();
         });
     }
