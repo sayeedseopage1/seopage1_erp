1746098bf5632df9cf3041f40055b2d75d346c28
tanvirmitul1 <mitulseopage1@gmail.com>
2024-07-08 14:28:59 +0600

    api add

--- resources/js/react/services/api/portfolioApiSlice.js
+++ resources/js/react/services/api/portfolioApiSlice.js
@@ -1,27 +1,54 @@
-import { apiSlice } from "./apiSlice";            
-
+import { apiSlice } from "./apiSlice";
+const _token = document
+    .querySelector("meta[name='csrf-token']")
+    .getAttribute("content");
 const portfolioApiSlice = apiSlice.injectEndpoints({
     endpoints: (build) => ({
         getPortfolioFilterMenuItems: build.query({
             query: () => `/account/portfolio/filter-menu`,
         }),
-        
+
         getPortfolioData: build.query({
-            query: (query) => `/account/portfolio/data${query}`
+            query: (query) => `/account/portfolio/data${query}`,
+            providesTags: ["ALL_PORTFOLIO"],
         }),
 
         getPortfolioDataById: build.query({
-            query: (id) => `/account/portfolio/${id}` 
-        })
-    })
-}) ;
+            query: (id) => `/account/portfolio/${id}`,
+        }),
+        storePortfolioRating: build.mutation({
+            query: (data) => ({
+                url: `/account/portfolio-rating-store`,
+                method: "POST",
+                body: {
+                    ...data,
+                    _token,
+                },
+            }),
 
- 
-export const { 
+            invalidatesTags: ["ALL_PORTFOLIO"],
+        }),
+        updatePortfolioRating: build.mutation({
+            query: (data) => ({
+                url: `/account/portfolio-rating-update`,
+                method: "POST",
+                body: {
+                    ...data,
+                    _token,
+                },
+            }),
+
+            invalidatesTags: ["ALL_PORTFOLIO"],
+        }),
+    }),
+});
+
+export const {
     useGetPortfolioFilterMenuItemsQuery,
     useLazyGetPortfolioFilterMenuItemsQuery,
     useGetPortfolioDataQuery,
     useLazyGetPortfolioDataQuery,
-    useLazyGetPortfolioDataByIdQuery
- } = portfolioApiSlice;
-
+    useLazyGetPortfolioDataByIdQuery,
+    useStorePortfolioRatingMutation,
+    useUpdatePortfolioRatingMutation,
+} = portfolioApiSlice;
