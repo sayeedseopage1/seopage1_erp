42e664defa1ac50c269c93cf1cff64ede100c0ee
mahfujseopage1 <mahfujar@seopage1.net>
2024-07-08 17:19:54 +0600

    [Change] condition added on lead page button

--- resources/js/react/projects/components/shared/PageNavigateButtons.jsx
+++ resources/js/react/projects/components/shared/PageNavigateButtons.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+// Components - Global
+import Switch from "../../../global/Switch";
+
 // style
 import style from "../sections/styles/dashboardHeaderSection.module.css";
 
@@ -10,24 +13,43 @@ import Button from "../ui/customComponents/Button/Button";
 const PageNavigateButtons = ({ navigateData, className = "" }) => {
     return (
         <div className={`${className}`}>
-            <Button
-                onClick={() => window.open(`/account/contracts/${navigateData?.deal_id}`, "_blank")}
-                className={`${style?.dashboardHeaderButton}`}
-            >
-                Won Deal
-            </Button>
-            <Button
-                onClick={() => window.open(`/account/deals/${navigateData?.deal?.deal_stage?.id}`, "_blank")}
-                className={`${style?.dashboardHeaderButton} ml-2`}
-            >
-                Deal Page
-            </Button>
-            <Button
-                onClick={() => window.open(`/account/leads/${navigateData?.deal?.lead_id}`, "_blank")}
-                className={`${style?.dashboardHeaderButton} ml-2`}
-            >
-                Lead Page
-            </Button>
+            <Switch>
+                <Button
+                    onClick={() =>
+                        window.open(
+                            `/account/contracts/${navigateData?.deal_id}`,
+                            "_blank"
+                        )
+                    }
+                    className={`${style?.dashboardHeaderButton}`}
+                >
+                    Won Deal
+                </Button>
+                <Button
+                    onClick={() =>
+                        window.open(
+                            `/account/deals/${navigateData?.deal?.deal_stage?.id}`,
+                            "_blank"
+                        )
+                    }
+                    className={`${style?.dashboardHeaderButton} ml-2`}
+                >
+                    Deal Page
+                </Button>
+                <Switch.Case condition={navigateData?.deal?.lead_id}>
+                    <Button
+                        onClick={() =>
+                            window.open(
+                                `/account/leads/${navigateData?.deal?.lead_id}`,
+                                "_blank"
+                            )
+                        }
+                        className={`${style?.dashboardHeaderButton} ml-2`}
+                    >
+                        Lead Page
+                    </Button>
+                </Switch.Case>
+            </Switch>
         </div>
     );
 };
