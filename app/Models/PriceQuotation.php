c563053eca9c643345d306d86bb967e55151b9f4
Saidul Islam <saidul@seopage1.net>
2024-07-12 11:43:46 +0600

    Some logic added to price quotations

--- app/Models/PriceQuotation.php
+++ app/Models/PriceQuotation.php
@@ -10,6 +10,7 @@ class PriceQuotation extends Model
     use HasFactory;
 
     protected $fillable = [
+        'serial_no',
         'deal_stage_id',
         'project_cms_id',
         'project_niche_id',
@@ -17,12 +18,17 @@ class PriceQuotation extends Model
         'no_of_secondary_pages',
         'no_of_major_functionalities',
         'risk_factor',
+        'total_hours_of_primary_page',
+        'total_hours_of_secondary_page',
+        'total_hours_of_major_functionality',
         'total_hours_of_others_works',
+        'total_calculated_hours',
         'currency_id',
         'deadline_type',
-        'deadline',
+        'no_of_days',
         'platform_account_id',
-        'calculated_budget',
-        'project_budget'
+        'calculated_actual_budget',
+        'calculated_usd_budget',
+        'project_budget',
     ];
 }
