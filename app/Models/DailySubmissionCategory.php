f7c6f4ace5168cafe7ac2744ca6f6874892a04d9
mdrayhanseopage1 <rayhan@seopage1.net>
2024-07-18 16:16:37 +0600

    optimize the query

--- app/Models/DailySubmissionCategory.php
+++ app/Models/DailySubmissionCategory.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class DailySubmissionCategory extends Model
 {
     use HasFactory;
+    public function dailySubmissionFields()
+    {
+        return $this->hasMany(DailySubmissionField::class, 'daily_submission_category_id');
+    }
 }
