64590011c6053ab58af3b4fe3fe1d5896dab39b7
mdrayhanseopage1 <rayhan@seopage1.net>
2024-07-18 16:23:28 +0600

    removed old code

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -7671,26 +7671,7 @@ class TaskController extends AccountBaseController
             ]);
         }
     }
-    // public function todaySubmissionData()
-    // {
-    //     $categories = DailySubmissionCategory::select('daily_submission_categories.*')
-    //         ->leftJoin('daily_submission_fields', 'daily_submission_fields.daily_submission_category_id', '=', 'daily_submission_categories.id')
-    //         ->groupBy('daily_submission_categories.id')
-    //         ->get()
-    //         ->map(function($category) {
-    //             $category->category_fields = DailySubmissionField::select('daily_submission_fields.*', 'daily_submission_categories.name as category_name')
-    //                 ->leftJoin('daily_submission_categories', 'daily_submission_categories.id', '=', 'daily_submission_fields.daily_submission_category_id')
-    //                 ->where('daily_submission_fields.daily_submission_category_id', $category->id)
-    //                 ->get();
-    //             return $category;
-    //         });
-
-    //     return response()->json([
-    //         'categories' => $categories,
-    //         'comments' => '1 - text, 2 - url, 3 - number, 4 - file, 5 - longText, 6 - list',
-    //         'status'=> 200
-    //     ]);
-    // }
+    
     public function todaySubmissionData()
     {
         $categories = DailySubmissionCategory::with('dailySubmissionFields')
