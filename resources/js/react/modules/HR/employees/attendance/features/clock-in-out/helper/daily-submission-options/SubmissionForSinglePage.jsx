11cb88fdf6e56dd6c233a5efbf52674674870ca7
tanvirmitul1 <mitulseopage1@gmail.com>
2024-07-27 09:44:04 +0600

    single page add

--- resources/js/react/modules/HR/employees/attendance/features/clock-in-out/helper/daily-submission-options/SubmissionForSinglePage.jsx
+++ resources/js/react/modules/HR/employees/attendance/features/clock-in-out/helper/daily-submission-options/SubmissionForSinglePage.jsx
@@ -1,66 +1,187 @@
-import React from "react";
-import { InputItem } from "../../DailySubmissionUI";
+import React, { useEffect, useState } from "react";
+import {
+    InputItem,
+    SectionContainer,
+    SectionTitle,
+} from "../../DailySubmissionUI";
 
 import SectionBuilding from "./options/SectionBuilding";
 import SubmissionDropDown from "./SubmissionDropDown";
+import MobileScreenResponsive from "./options/MobileScreenResponsive";
+import Revision from "./options/Revision";
+import BugFixing from "./options/BugFixing";
+import FunctionalityDevelopment from "./options/FunctionalityDevelopment";
+import SpeedOptimization from "./options/SpeedOptimization";
+import DomainHostingConnection from "./options/DomainHostingConnection";
+import Migration from "./options/Migration";
+import ProductUploading from "./options/ProductUploading";
+import BlogUploading from "./options/BlogUploading";
+import CloningPage from "./options/CloningPage";
+import Others from "./options/Others";
+import { dailySubmissionStaticData } from "../../../../../../../../utils/daily-submission-categories";
+import Button from "../../../../../../../../global/Button";
+import { CiCircleMinus, CiCirclePlus } from "react-icons/ci";
+const SubmissionForSinglePage = ({
+    pageNumber,
+    dailySubPagesData,
+    setDailySubPagesData,
+    handleDailySubPagesData,
+}) => {
+    const [numberOfSections, setNumberOfSections] = useState(1);
+    const [selectedCategories, setSelectedCategories] = useState([]);
+    const [pageUrl, setPageUrl] = useState("");
+    const [singlePageData, setSinglePageData] = useState([]);
 
-const SubmissionForSinglePage = ({ id, handleChange, value }) => {
-    const [selectedCategoryDropDown, setSelectedCategoryDropDown] =
-        React.useState({
-            id: 1,
-            name: "Section building",
+    const SectionNumberArray = Array.from(
+        { length: numberOfSections },
+        (_, index) => index
+    );
+
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        setCategories(dailySubmissionStaticData?.categories);
+    }, []);
+
+    const handleCategoryChange = (sectionIndex, category) => {
+        setSelectedCategories((prev) => {
+            const newCategories = [...prev];
+            newCategories[sectionIndex] = category;
+            return newCategories;
         });
-    console.log(selectedCategoryDropDown, "selectedCategoryDropDown");
+    };
+
+    const getComponentForCategory = (index, category, pageNumber) => {
+        switch (category?.id) {
+            case 1:
+                return (
+                    <SectionBuilding
+                        sectionId={index}
+                        pageNumber={pageNumber}
+                        category={category}
+                        setSinglePageData={setSinglePageData}
+                    />
+                );
+            case 2:
+                return <MobileScreenResponsive />;
+            case 3:
+                return <Revision />;
+            case 4:
+                return <BugFixing />;
+            case 5:
+                return <FunctionalityDevelopment />;
+            case 6:
+                return <SpeedOptimization />;
+            case 7:
+                return <DomainHostingConnection />;
+            case 8:
+                return <Migration />;
+            case 9:
+                return <ProductUploading />;
+            case 10:
+                return <BlogUploading />;
+            case 11:
+                return <CloningPage />;
+            case 12:
+                return <Others />;
+            default:
+                return null;
+        }
+    };
+
+    console.log("page url", pageUrl);
+    console.log("single page data", singlePageData);
     return (
         <div style={{ marginBottom: "50px", marginTop: "30px" }}>
-            <div style={{ fontWeight: "bold" }}>
-                Enter the page URL for Page #{id}
+            <div style={{ color: "#979797" }}>
+                Enter the page URL for Page # {""}
+                {pageNumber}
             </div>
             <InputItem
-                width="50%"
-                placeHolder="Provide URL Link.."
-                value={value}
-                onChange={(e) => handleChange(id, e.target.value)}
+                width="65%"
+                placeHolder=""
+                value={pageUrl}
+                onChange={(e) => setPageUrl(e.target.value)}
             />
 
-            <SubmissionDropDown
-                selectedCategoryDropDown={selectedCategoryDropDown}
-                setSelectedCategoryDropDown={setSelectedCategoryDropDown}
-            />
+            <strong>What did you do on this page</strong>
 
-            <>
-                {selectedCategoryDropDown?.id === 1 && <SectionBuilding />}
+            {SectionNumberArray.map((index) => (
+                <div style={{ marginBottom: "10px" }} key={index}>
+                    <SubmissionDropDown
+                        categories={categories}
+                        setCategories={setCategories}
+                        selectedCategory={selectedCategories[index]}
+                        onCategoryChange={(category) =>
+                            handleCategoryChange(index, category)
+                        }
+                    />
+                    {selectedCategories[index] !== undefined && (
+                        <SectionContainer>
+                            <SectionTitle>
+                                Section {index + 1}:{" "}
+                                {selectedCategories[index]?.name}
+                            </SectionTitle>
+                            {getComponentForCategory(
+                                index,
+                                selectedCategories[index],
+                                pageNumber
+                            )}
+                        </SectionContainer>
+                    )}
+                </div>
+            ))}
 
-                {selectedCategoryDropDown?.id === 2 && (
+            {SectionNumberArray?.length > 1 && (
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        width: "60%",
+                        fontSize: "18px",
+                        cursor: "pointer",
+                        margin: "0px 20px",
+                    }}
+                >
                     <div>
-                        <h4 style={{ fontWeight: "bold" }}>
-                            Mobile/other screen responsiveness
-                        </h4>
-                        <div>
-                            <div>Enter input 1</div>
-                            <InputItem
-                                width="100%"
-                                placeHolder="input 1.."
-                                value={value}
-                                onChange={(e) =>
-                                    handleChange(id, e.target.value)
-                                }
-                            />
-                        </div>
-                        <div>
-                            <div>Enter input 2</div>
-                            <InputItem
-                                width="100%"
-                                placeHolder="input 2.."
-                                value={value}
-                                onChange={(e) =>
-                                    handleChange(id, e.target.value)
-                                }
-                            />
+                        <div
+                            style={{ color: "red" }}
+                            onClick={() =>
+                                setNumberOfSections((prev) => prev - 1)
+                            }
+                        >
+                            <span>
+                                <CiCircleMinus size={25} />
+                            </span>{" "}
+                            <span>Remove the section</span>
                         </div>
                     </div>
-                )}
-            </>
+                    <div>
+                        <div
+                            style={{ color: "Blue" }}
+                            onClick={() =>
+                                setNumberOfSections((prev) => prev + 1)
+                            }
+                        >
+                            <span>
+                                <CiCirclePlus size={25} />
+                            </span>{" "}
+                            <span>Add New section</span>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+            {SectionNumberArray.length < 2 && (
+                <div>
+                    <Button
+                        size="md"
+                        onClick={() => setNumberOfSections((prev) => prev + 1)}
+                    >
+                        Add New
+                    </Button>
+                </div>
+            )}
         </div>
     );
 };
