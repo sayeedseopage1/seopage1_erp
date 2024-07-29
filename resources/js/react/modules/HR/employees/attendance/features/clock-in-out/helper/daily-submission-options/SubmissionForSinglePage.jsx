cb17e8b2f656c22a050f2ede3de806dbbbdbb6f2
tanvirmitul1 <mitulseopage1@gmail.com>
2024-07-29 16:24:22 +0600

    single page button fix

--- resources/js/react/modules/HR/employees/attendance/features/clock-in-out/helper/daily-submission-options/SubmissionForSinglePage.jsx
+++ resources/js/react/modules/HR/employees/attendance/features/clock-in-out/helper/daily-submission-options/SubmissionForSinglePage.jsx
@@ -21,27 +21,46 @@ import Others from "./options/Others";
 import { dailySubmissionStaticData } from "../../../../../../../../utils/daily-submission-categories";
 import Button from "../../../../../../../../global/Button";
 import { CiCircleMinus, CiCirclePlus } from "react-icons/ci";
+
 const SubmissionForSinglePage = ({
+    numberOfPages,
     pageNumber,
     dailySubPagesData,
     setDailySubPagesData,
     handleDailySubPagesData,
 }) => {
-    const [numberOfSections, setNumberOfSections] = useState(1);
-    const [selectedCategories, setSelectedCategories] = useState([]);
-    const [pageUrl, setPageUrl] = useState("");
-    const [singlePageData, setSinglePageData] = useState([]);
-
-    const SectionNumberArray = Array.from(
-        { length: numberOfSections },
-        (_, index) => index
-    );
-
     const [categories, setCategories] = useState([]);
-
     useEffect(() => {
         setCategories(dailySubmissionStaticData?.categories);
     }, []);
+    const [sections, setSections] = useState([{ id: 0 }]);
+    const [selectedCategories, setSelectedCategories] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [pageUrl, setPageUrl] = useState("");
+    const [singlePageData, setSinglePageData] = useState([]);
+
+    useEffect(() => {
+        setCurrentPage(pageNumber);
+    }, []);
+
+    useEffect(() => {
+        if (!singlePageData) return;
+
+        setDailySubPagesData((prev) => {
+            const updatedPageData = [...(prev.pageData || [])];
+            updatedPageData[currentPage - 1] = singlePageData;
+
+            return {
+                ...prev,
+                totalPage: pageNumber,
+                pageData: updatedPageData,
+            };
+        });
+    }, [singlePageData, currentPage]);
+
+    useEffect(() => {
+        setDailySubPagesData({});
+    }, [numberOfPages]);
 
     const handleCategoryChange = (sectionIndex, category) => {
         setSelectedCategories((prev) => {
@@ -56,6 +75,7 @@ const SubmissionForSinglePage = ({
             case 1:
                 return (
                     <SectionBuilding
+                        pageUrl={pageUrl}
                         sectionId={index}
                         pageNumber={pageNumber}
                         category={category}
@@ -63,34 +83,142 @@ const SubmissionForSinglePage = ({
                     />
                 );
             case 2:
-                return <MobileScreenResponsive />;
+                return (
+                    <MobileScreenResponsive
+                        pageUrl={pageUrl}
+                        sectionId={index}
+                        pageNumber={pageNumber}
+                        category={category}
+                        setSinglePageData={setSinglePageData}
+                    />
+                );
             case 3:
-                return <Revision />;
+                return (
+                    <Revision
+                        pageUrl={pageUrl}
+                        sectionId={index}
+                        pageNumber={pageNumber}
+                        category={category}
+                        setSinglePageData={setSinglePageData}
+                    />
+                );
             case 4:
-                return <BugFixing />;
+                return (
+                    <BugFixing
+                        pageUrl={pageUrl}
+                        sectionId={index}
+                        pageNumber={pageNumber}
+                        category={category}
+                        setSinglePageData={setSinglePageData}
+                    />
+                );
             case 5:
-                return <FunctionalityDevelopment />;
+                return (
+                    <FunctionalityDevelopment
+                        pageUrl={pageUrl}
+                        sectionId={index}
+                        pageNumber={pageNumber}
+                        category={category}
+                        setSinglePageData={setSinglePageData}
+                    />
+                );
             case 6:
-                return <SpeedOptimization />;
+                return (
+                    <SpeedOptimization
+                        pageUrl={pageUrl}
+                        sectionId={index}
+                        pageNumber={pageNumber}
+                        category={category}
+                        setSinglePageData={setSinglePageData}
+                    />
+                );
             case 7:
-                return <DomainHostingConnection />;
+                return (
+                    <DomainHostingConnection
+                        pageUrl={pageUrl}
+                        sectionId={index}
+                        pageNumber={pageNumber}
+                        category={category}
+                        setSinglePageData={setSinglePageData}
+                    />
+                );
             case 8:
-                return <Migration />;
+                return (
+                    <Migration
+                        pageUrl={pageUrl}
+                        sectionId={index}
+                        pageNumber={pageNumber}
+                        category={category}
+                        setSinglePageData={setSinglePageData}
+                    />
+                );
             case 9:
-                return <ProductUploading />;
+                return (
+                    <ProductUploading
+                        pageUrl={pageUrl}
+                        sectionId={index}
+                        pageNumber={pageNumber}
+                        category={category}
+                        setSinglePageData={setSinglePageData}
+                    />
+                );
             case 10:
-                return <BlogUploading />;
+                return (
+                    <BlogUploading
+                        pageUrl={pageUrl}
+                        sectionId={index}
+                        pageNumber={pageNumber}
+                        category={category}
+                        setSinglePageData={setSinglePageData}
+                    />
+                );
             case 11:
-                return <CloningPage />;
+                return (
+                    <CloningPage
+                        pageUrl={pageUrl}
+                        sectionId={index}
+                        pageNumber={pageNumber}
+                        category={category}
+                        setSinglePageData={setSinglePageData}
+                    />
+                );
             case 12:
-                return <Others />;
+                return (
+                    <Others
+                        pageUrl={pageUrl}
+                        sectionId={index}
+                        pageNumber={pageNumber}
+                        category={category}
+                        setSinglePageData={setSinglePageData}
+                    />
+                );
             default:
                 return null;
         }
     };
 
+    const handleAddSection = () => {
+        setSections((prevSections) => [
+            ...prevSections,
+            { id: prevSections.length },
+        ]);
+    };
+
+    const handleRemoveSection = (id) => {
+        setSections((prevSections) =>
+            prevSections.filter((section) => section.id !== id)
+        );
+        setSelectedCategories((prevCategories) =>
+            prevCategories.filter((_, index) => index !== id)
+        );
+        setSinglePageData((prevData) =>
+            prevData.filter((data) => data.id !== `${id}`)
+        );
+    };
+
     console.log("page url", pageUrl);
     console.log("single page data", singlePageData);
+
     return (
         <div style={{ marginBottom: "50px", marginTop: "30px" }}>
             <div style={{ color: "#979797" }}>
@@ -106,8 +234,8 @@ const SubmissionForSinglePage = ({
 
             <strong>What did you do on this page</strong>
 
-            {SectionNumberArray.map((index) => (
-                <div style={{ marginBottom: "10px" }} key={index}>
+            {sections.map((section, index) => (
+                <div key={section.id}>
                     <SubmissionDropDown
                         categories={categories}
                         setCategories={setCategories}
@@ -129,59 +257,74 @@ const SubmissionForSinglePage = ({
                             )}
                         </SectionContainer>
                     )}
+                    {selectedCategories[index]?.id == 1
+                        ? index === sections.length - 1 && (
+                              <div
+                                  style={{
+                                      display: "flex",
+                                      justifyContent: "space-between",
+                                      width: "60%",
+                                      fontSize: "18px",
+                                      cursor: "pointer",
+                                      margin: "0px 20px",
+                                  }}
+                              >
+                                  <div>
+                                      <div
+                                          style={{ color: "red" }}
+                                          onClick={() =>
+                                              handleRemoveSection(section.id)
+                                          }
+                                      >
+                                          <span>
+                                              <CiCircleMinus size={25} />
+                                          </span>{" "}
+                                          <span>Remove the section</span>
+                                      </div>
+                                  </div>
+                                  <div>
+                                      <div
+                                          style={{ color: "Blue" }}
+                                          onClick={handleAddSection}
+                                      >
+                                          <span>
+                                              <CiCirclePlus size={25} />
+                                          </span>{" "}
+                                          <span>Add New section</span>
+                                      </div>
+                                  </div>
+                              </div>
+                          )
+                        : sections.length > 1 &&
+                          index === sections.length - 1 && (
+                              <div
+                                  style={{
+                                      display: "flex",
+                                      justifyContent: "flex-end",
+                                      width: "60%",
+                                      fontSize: "18px",
+                                      cursor: "pointer",
+                                      margin: "0px 10px",
+                                  }}
+                              >
+                                  <Button
+                                      size="sm"
+                                      variant="danger"
+                                      onClick={() =>
+                                          handleRemoveSection(section.id)
+                                      }
+                                  >
+                                      Remove
+                                  </Button>
+                              </div>
+                          )}
                 </div>
             ))}
-
-            {SectionNumberArray?.length > 1 && (
-                <div
-                    style={{
-                        display: "flex",
-                        justifyContent: "space-between",
-                        width: "60%",
-                        fontSize: "18px",
-                        cursor: "pointer",
-                        margin: "0px 20px",
-                    }}
-                >
-                    <div>
-                        <div
-                            style={{ color: "red" }}
-                            onClick={() =>
-                                setNumberOfSections((prev) => prev - 1)
-                            }
-                        >
-                            <span>
-                                <CiCircleMinus size={25} />
-                            </span>{" "}
-                            <span>Remove the section</span>
-                        </div>
-                    </div>
-                    <div>
-                        <div
-                            style={{ color: "Blue" }}
-                            onClick={() =>
-                                setNumberOfSections((prev) => prev + 1)
-                            }
-                        >
-                            <span>
-                                <CiCirclePlus size={25} />
-                            </span>{" "}
-                            <span>Add New section</span>
-                        </div>
-                    </div>
-                </div>
-            )}
-
-            {SectionNumberArray.length < 2 && (
-                <div>
-                    <Button
-                        size="md"
-                        onClick={() => setNumberOfSections((prev) => prev + 1)}
-                    >
-                        Add New
-                    </Button>
-                </div>
-            )}
+            <div style={{ marginTop: "10px" }}>
+                <Button size="md" onClick={handleAddSection}>
+                    Add New
+                </Button>
+            </div>
         </div>
     );
 };
